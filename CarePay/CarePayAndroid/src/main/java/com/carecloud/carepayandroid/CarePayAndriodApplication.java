743113c57986c478a9390f1b336c73317476c2ec
Md Jahirul Islam Bhuiyan <md.jahirul.islam.bhuiyan.2014@gmail.com>
2016-08-22 18:15:06 -0400

    SHMRK-209 - Additional folders and files added
    http://jira.carecloud.com/browse/SHMRK-209

---
+++ CarePay/CarePayAndroid/src/main/java/com/carecloud/carepayandroid/CarePayAndriodApplication.java
@@ -0,0 +1,9 @@
+package com.carecloud.carepayandroid;
+
+import android.app.Application;
+
+/**
+ * Created by Jahirul Bhuiyan on 8/22/2016.
+ */
+public class CarePayAndriodApplication extends Application {
+}
--- CarePay/CarePayAndroid/src/main/java/com/carecloud/carepayandroid/activities/LauncherActivity.java
+++ CarePay/CarePayAndroid/src/main/java/com/carecloud/carepayandroid/activities/LauncherActivity.java
@@ -1,13 +1,13 @@
-package com.carecloud.carepayandroid;
+package com.carecloud.carepayandroid.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launcher);
     }
 }
