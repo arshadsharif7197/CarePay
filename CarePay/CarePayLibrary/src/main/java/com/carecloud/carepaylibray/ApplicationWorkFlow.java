7eecd0c4f27de28f2497ada34e2deffb689f25c5
liviu-carecloud <liviu_socolovici@persistent.com>
2016-08-23 18:51:29 -0400

    included the ScreenModel factory

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/ApplicationWorkFlow.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/ApplicationWorkFlow.java
@@ -15,22 +15,30 @@ public class ApplicationWorkFlow {
     private ApplicationWorkFlow() {
         workFlowModel=new WorkFlowModel();
     }
+
     public static ApplicationWorkFlow Instance( ) {
         if(instance == null) {
             instance = new ApplicationWorkFlow();
         }
         return instance;
     }
-    public ScreenModel getSelectLanguageScreenModel() {
-        return workFlowModel.getSelectLanguageScreenModel();
-    }
 
+    public ScreenModel getSelectLanguageScreenModel() { return workFlowModel.getSelectLanguageScreenModel(); }
     public ScreenModel getLoginScreenModel() {
         return workFlowModel.getLoginScreenModel();
     }
-
     public ScreenModel getSignupScreenModel() {
         return workFlowModel.getSignupScreenModel();
     }
+    public ScreenModel getResponsabScreenModel() { return workFlowModel.getResponsabScreenModel(); }
+    public ScreenModel getDemographicsAddressScreenModel() { return workFlowModel.getDemographicsAddressScreenModel(); }
+    public ScreenModel getDemographicsDetailsScreenModel() { return workFlowModel.getDemographicsDetailsScreenModel(); }
+    public ScreenModel getDemographicDetailsEthicityScreenModel() { return workFlowModel.getDemographicsDetailsEthnicityScreenModel(); }
+    public ScreenModel getDemographicsDocumentsScreenModel() { return workFlowModel.getDemographicsDocumentsScreenModel(); }
+    public ScreenModel getInsuranceInfoScreenModel() { return workFlowModel.getInsuranceInfoScreenModel(); }
+    public ScreenModel getNewProfileScreenModel() { return workFlowModel.getNewProfileScreenModel(); }
+    public ScreenModel getDemographicsMoreDetailsScreenModel() { return workFlowModel.getDemographicsMoreDetailsScreenModel(); };
+    public ScreenModel getDemographicsMoreDetailsUpdatesScreenModel() { return workFlowModel.getDemographicsMoreDetailsUpdatesScreenModel(); };
+
 
 }
--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/models/WorkFlowModel.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/models/WorkFlowModel.java
@@ -8,10 +8,17 @@ import java.util.ArrayList;
 public class WorkFlowModel {
 
     private ScreenModel selectLanguageScreenModel;
-
     private ScreenModel loginScreenModel;
-
     private ScreenModel signupScreenModel;
+    private ScreenModel responsabScreenModel;
+    private ScreenModel demographicsAddressScreenModel;
+    private ScreenModel demographicsDetailsScreenModel;
+    private ScreenModel demographicsDetailsEthnicityScreenModel;
+    private ScreenModel demographicsDocumentsScreenModel;
+    private ScreenModel demographicsMoreDetailsScreenModel;
+    private ScreenModel demographicsMoreDetailsUpdatesScreenModel;
+    private ScreenModel insuranceInfoScreenModel;
+    private ScreenModel newProfileScreenModel;
 
 
     public ScreenModel getSelectLanguageScreenModel() {
@@ -60,5 +67,153 @@ public class WorkFlowModel {
 
     public void setSignupScreenModel(ScreenModel signupScreenModel) {
         this.signupScreenModel = signupScreenModel;
+    }
+
+    public ScreenModel getResponsabScreenModel() {
+        responsabScreenModel = new ScreenModel();
+        ArrayList<ScreenComponentModel> componentModels = new ArrayList<>();
+        componentModels.add(new ScreenComponentModel("doctor", "text", true));
+        componentModels.add(new ScreenComponentModel("label_cost", "text", true));
+        componentModels.add(new ScreenComponentModel("cost", "text", true));
+        componentModels.add(new ScreenComponentModel("label_prev_balance", "text", true));
+        componentModels.add(new ScreenComponentModel("prev_balance", "text", true));
+        componentModels.add(new ScreenComponentModel("label_insurance_copay", "text", true));
+        componentModels.add(new ScreenComponentModel("insurance_copay", "text", true));
+        componentModels.add(new ScreenComponentModel("sign_and_pay", "buttom", true));
+        responsabScreenModel.setComponentModels(componentModels);
+        return responsabScreenModel;
+    }
+
+    public void setResponsabScreenModel(ScreenModel responsabScreenModel) {
+        this.responsabScreenModel = responsabScreenModel;
+    }
+
+    public ScreenModel getDemographicsAddressScreenModel() {
+        demographicsAddressScreenModel = new ScreenModel();
+        ArrayList<ScreenComponentModel> componentModels = new ArrayList<>();
+        componentModels.add(new ScreenComponentModel("logo", "text", true));
+        componentModels.add(new ScreenComponentModel("phone", "phonenumber", true));
+        componentModels.add(new ScreenComponentModel("zip", "text", true));
+        componentModels.add(new ScreenComponentModel("address1", "inputtext", true));
+        componentModels.add(new ScreenComponentModel("address2", "inputtext", false));
+        componentModels.add(new ScreenComponentModel("city", "inputtext", true));
+        componentModels.add(new ScreenComponentModel("state", "inputtext", true));
+        componentModels.add(new ScreenComponentModel("next", "button", true));
+        demographicsAddressScreenModel.setComponentModels(componentModels);
+        return demographicsAddressScreenModel;
+    }
+
+    public void setDemographicsAddressScreenModel(ScreenModel demographicsAddressScreenModel) {
+        this.demographicsAddressScreenModel = demographicsAddressScreenModel;
+    }
+
+
+    public ScreenModel getDemographicsDetailsScreenModel() {
+        demographicsAddressScreenModel = new ScreenModel();
+        ArrayList<ScreenComponentModel> componentModels = new ArrayList<>();
+        componentModels.add(new ScreenComponentModel("logo", "text", true));
+        componentModels.add(new ScreenComponentModel("picture", "imageview", true));
+        componentModels.add(new ScreenComponentModel("label_race", "text", true));
+        componentModels.add(new ScreenComponentModel("choose_race", "button", true));
+        componentModels.add(new ScreenComponentModel("label_ethinicity", "text", true));
+        componentModels.add(new ScreenComponentModel("choose_ethnicity", "button", true));
+        componentModels.add(new ScreenComponentModel("label_pref_lang", "text", true));
+        componentModels.add(new ScreenComponentModel("pref_lang", "text", true));
+        return demographicsDetailsScreenModel;
+    }
+
+    public void setDemographicsDetailsScreenModel(ScreenModel demographicsDetailsScreenModel) {
+        this.demographicsDetailsScreenModel = demographicsDetailsScreenModel;
+    }
+
+
+    public ScreenModel getDemographicsDetailsEthnicityScreenModel() {
+        demographicsDetailsEthnicityScreenModel = new ScreenModel();
+        ArrayList<ScreenComponentModel> componentModels = new ArrayList<>();
+        componentModels.add(new ScreenComponentModel("ethnicity", "listview", true));
+        return demographicsDetailsEthnicityScreenModel;
+    }
+
+    public void setDemographicsDetailsEthnicityScreenModel(ScreenModel demographicsDetailsEthnicityScreenModel) {
+        this.demographicsDetailsEthnicityScreenModel = demographicsDetailsEthnicityScreenModel;
+    }
+
+    public ScreenModel getDemographicsDocumentsScreenModel() {
+        demographicsDocumentsScreenModel = new ScreenModel();
+        ArrayList<ScreenComponentModel> componentModels = new ArrayList<>();
+        componentModels.add(new ScreenComponentModel("logo", "text", true));
+        componentModels.add(new ScreenComponentModel("thumbnail_license", "imageview", true));
+        componentModels.add(new ScreenComponentModel("scan_license", "button", true));
+        componentModels.add(new ScreenComponentModel("license_number", "text", true));
+        componentModels.add(new ScreenComponentModel("label_license_state", "text", true));
+        componentModels.add(new ScreenComponentModel("select", "button", true));
+        componentModels.add(new ScreenComponentModel("label_have_insurance", "text", true));
+        componentModels.add(new ScreenComponentModel("have_insurance", "togglebutton", true));
+        componentModels.add(new ScreenComponentModel("next", "button", true));
+        componentModels.add(new ScreenComponentModel("thumbnail_insurance", "imageview", false));
+        componentModels.add(new ScreenComponentModel("scan_insurance", "button", false));
+        componentModels.add(new ScreenComponentModel("insurance_info_1", "text", false));
+        componentModels.add(new ScreenComponentModel("insurance_info_2", "text", false));
+        componentModels.add(new ScreenComponentModel("insurance_info_3", "text", false));
+        componentModels.add(new ScreenComponentModel("check_insurance", "button", false));
+        demographicsDocumentsScreenModel.setComponentModels(componentModels);
+        return demographicsDocumentsScreenModel;
+    }
+
+    public void setDemographicsDocumentsScreenModel(ScreenModel demographicsDocumentsScreenModel) {
+        this.demographicsDocumentsScreenModel = demographicsDocumentsScreenModel;
+    }
+
+    public ScreenModel getDemographicsMoreDetailsScreenModel() {
+        demographicsMoreDetailsScreenModel = new ScreenModel();
+        ArrayList<ScreenComponentModel> componentModels = new ArrayList<>();
+        componentModels.add(new ScreenComponentModel("logo", "text", true));
+        componentModels.add(new ScreenComponentModel("sub-logo", "text", true));
+        componentModels.add(new ScreenComponentModel("label_want_updates", "text", true));
+        componentModels.add(new ScreenComponentModel("want_updates", "togglebutton", true));
+        componentModels.add(new ScreenComponentModel("add", "button", true));
+        componentModels.add(new ScreenComponentModel("do_later", "button", true));
+        demographicsMoreDetailsScreenModel.setComponentModels(componentModels);
+        return demographicsMoreDetailsScreenModel;
+    }
+
+    public void setDemographicsMoreDetailsScreenModel(ScreenModel demographicsMoreDetailsScreenModel) {
+        this.demographicsMoreDetailsScreenModel = demographicsMoreDetailsScreenModel;
+    }
+
+    public ScreenModel getDemographicsMoreDetailsUpdatesScreenModel() {
+        demographicsMoreDetailsUpdatesScreenModel = new ScreenModel();
+        ArrayList<ScreenComponentModel> componentModels = new ArrayList<>();
+        componentModels.add(new ScreenComponentModel("updates", "listview", true));
+        demographicsMoreDetailsUpdatesScreenModel.setComponentModels(componentModels);
+        return demographicsMoreDetailsUpdatesScreenModel;
+    }
+
+    public void setDemographicsMoreDetailsUpdatesScreenModel(ScreenModel demographicsMoreDetailsUpdatesScreenModel) {
+        this.demographicsMoreDetailsUpdatesScreenModel = demographicsMoreDetailsUpdatesScreenModel;
+    }
+
+    public ScreenModel getInsuranceInfoScreenModel() {
+        insuranceInfoScreenModel = new ScreenModel();
+        ArrayList<ScreenComponentModel> componentModels = new ArrayList<>();
+        // todo add components when mock-up available
+        insuranceInfoScreenModel.setComponentModels(componentModels);
+        return insuranceInfoScreenModel;
+    }
+
+    public void setInsuranceInfoScreenModel(ScreenModel insuranceInfoScreenModel) {
+        this.insuranceInfoScreenModel = insuranceInfoScreenModel;
+    }
+
+    public ScreenModel getNewProfileScreenModel() {
+        newProfileScreenModel = new ScreenModel();
+        ArrayList<ScreenComponentModel> componentModels = new ArrayList<>();
+        // todo add components when mock-up available
+        newProfileScreenModel.setComponentModels(componentModels);
+        return newProfileScreenModel;
+    }
+
+    public void setNewProfileScreenModel(ScreenModel newProfileScreenModel) {
+        this.newProfileScreenModel = newProfileScreenModel;
     }
 }
