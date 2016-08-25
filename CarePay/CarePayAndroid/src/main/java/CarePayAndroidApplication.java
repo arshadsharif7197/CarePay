b7855035c052d8abc10656a9c2eb0e24b264ff69
liviu-carecloud <liviu_socolovici@persistent.com>
2016-08-25 15:16:51 -0400

    [SHMRk-235] created demo activity

---
+++ CarePay/CarePayAndroid/src/main/java/CarePayAndroidApplication.java
@@ -0,0 +1,12 @@
+import android.app.Application;
+
+import retrofit2.http.PUT;
+
+/**
+ * Created by lsoco_user on 8/25/2016.
+ */
+public class CarePayAndroidApplication extends Application {
+    public CarePayAndroidApplication() {
+        super();
+    }
+}
