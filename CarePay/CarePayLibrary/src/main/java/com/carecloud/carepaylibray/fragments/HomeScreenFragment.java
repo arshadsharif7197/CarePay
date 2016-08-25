49ecc4f931a8bee27e9854701cc9c94d7bce00e6
Md Jahirul Islam Bhuiyan <md.jahirul.islam.bhuiyan.2014@gmail.com>
2016-08-25 13:52:42 -0400

    SHMRK-217 - drawable icons added
    http://jira.carecloud.com/browse/SHMRK-217

---
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/fragments/HomeScreenFragment.java
@@ -0,0 +1,24 @@
+package com.carecloud.carepaylibray.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jahirul Bhuiyan on 8/25/2016.
+ */
+public class HomeScreenFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return null;
+    }
+}
