9ae100212f5fb65be93b377af0c69c5631fae4d6
liviu-carecloud <liviu_socolovici@persistent.com>
2016-08-26 12:05:26 -0400

    [SHMRk-235] updated SignUp Screen

--- CarePay/CarePayAndroid/src/main/java/com/carecloud/carepayandroid/activities/SignUpActivity.java
+++ CarePay/CarePayAndroid/src/main/java/com/carecloud/carepayandroid/activities/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.carecloud.carepayandroid.activities;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.carecloud.carepayandroid.R;
 import com.carecloud.carepaylibray.fragments.SignUpFragment;
@@ -14,10 +15,13 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        toolbar.setTitle("New Account");
+
         SignUpFragment signUpFragment = new SignUpFragment();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.add(R.id.signup_frame_holder,signUpFragment).commit();
-
-
+        ft.replace(R.id.signup_frame_layout, signUpFragment).commit();
     }
 }
--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/fragments/SignUpFragment.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/fragments/SignUpFragment.java
@@ -1,10 +1,8 @@
 package com.carecloud.carepaylibray.fragments;
 
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.text.InputType;
@@ -32,125 +30,93 @@ import java.util.regex.Pattern;
  * Created by anil_kairamkonda on 8/24/2016.
  */
 public class SignUpFragment extends Fragment {
-    ScreenModel screenModel;
-
-
-    View view;
-    private TextView et1;
+    private ScreenModel screenModel;
+    private TextView editText;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         LinearLayout.LayoutParams matchWidthParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         LinearLayout parent = new LinearLayout(getActivity());
         parent.setLayoutParams(matchWidthParams);
         parent.setOrientation(LinearLayout.VERTICAL);
         parent.setPadding(20, 20, 0, 20);
 
-        LinearLayout.LayoutParams LayoutParamsview = new AppBarLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-
         screenModel = ApplicationWorkflow.Instance().getSignupScreenModel();
-
         getActivity().setTitle(screenModel.getName());
 
         int index = 0;
-
-
         for (final ScreenComponentModel componentModel : screenModel.getComponentModels()) {
-
             if (componentModel.getType().equals("inputtext") || componentModel.getType().equals("email") || componentModel.getType().equals("password")) {
 
-                TextInputLayout fullnameTextInputLayout = new TextInputLayout(getActivity());
+                TextInputLayout fullNameTextInputLayout = new TextInputLayout(getActivity());
+                editText = new EditText(getActivity());
 
-//                LinearLayout.LayoutParams l2=new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//                l2.setMargins(14,0,0,0);
+                LinearLayout.LayoutParams signUpParentLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                signUpParentLayoutParams.setMargins(17, 268, 0, 0);
 
-                //et1.setLayoutParams(l2);
-
-
-                et1 = new EditText(getActivity());
-
-                LinearLayout.LayoutParams la2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                la2.setMargins(17, 268, 0, 0);
-
-                et1.setLayoutParams(la2);
+                editText.setLayoutParams(signUpParentLayoutParams);
                 if (componentModel.getType().equals("password")) {
-                    et1.setInputType(InputType.TYPE_CLASS_TEXT |
+
+                    editText.setInputType(InputType.TYPE_CLASS_TEXT |
                             InputType.TYPE_TEXT_VARIATION_PASSWORD);
                 }
 
+                editText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 17);
+                editText.setTextColor(Color.parseColor("#1f9bde"));
+                editText.setHintTextColor(Color.parseColor("#78909c"));
+                editText.setLayoutParams(matchWidthParams);
 
-                et1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 17);
-                et1.setTextColor(Color.parseColor("#1f9bde"));
-                et1.setLayoutParams(matchWidthParams);
-                et1.setTypeface(et1.getTypeface(), Typeface.BOLD);
+                fullNameTextInputLayout.setHint(componentModel.getLabel());
 
-                fullnameTextInputLayout.setHint(componentModel.getLabel());
-
-                parent.addView(fullnameTextInputLayout, index, matchWidthParams);
-                fullnameTextInputLayout.addView(et1);
+                parent.addView(fullNameTextInputLayout, index, matchWidthParams);
+                fullNameTextInputLayout.addView(editText);
                 index++;
-
 
             } else {
                 if (componentModel.getType().equals("button")) {
-                    LinearLayout.LayoutParams la = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                    la.setMargins(1, 99, 17, 17);
-                    Button btn1 = new Button(getActivity());
+                    LinearLayout.LayoutParams signUpButtonLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    signUpButtonLayoutParams.setMargins(0, 86, 0, 0);
+                    Button signUpButton = new Button(getActivity());
+                    signUpButton.setLayoutParams(signUpButtonLayoutParams);
+                    signUpButton.setText(componentModel.getLabel());
+                    signUpButton.setBackgroundColor(Color.parseColor("#1f9bde"));
+                    signUpButton.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+                    signUpButton.setTextColor(Color.parseColor("#ffffff"));
 
-                    btn1.setLayoutParams(la);
-                    btn1.setText(componentModel.getLabel());
-                    btn1.setBackgroundColor(Color.parseColor("#1f9bde"));
-                    btn1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
-
-                    btn1.setTextColor(Color.parseColor("#ffffff"));
-
-                    btn1.setOnClickListener(new View.OnClickListener() {
+                    signUpButton.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
                             for (final ScreenComponentModel componentModel : screenModel.getComponentModels()) {
                                 if (componentModel.getType().equals("email") || componentModel.getType().equals("password") && componentModel.isRequired()) {
-                                    if (TextUtils.isEmpty(et1.getText().toString())) {
+                                    if (TextUtils.isEmpty(editText.getText().toString())) {
                                         Toast.makeText(getActivity(), "Fields can not be empty", Toast.LENGTH_LONG).show();
                                         break;
-                                    } else if (componentModel.getType().equals("email") && !isEmailValid(et1.getText().toString())) {
-                                        Toast.makeText(getActivity(), "Please enter a valid email id", Toast.LENGTH_LONG).show();
+                                    } else if (componentModel.getType().equals("email") && !isEmailValid(editText.getText().toString())) {
+                                        Toast.makeText(getActivity(), "Email field is not valid", Toast.LENGTH_LONG).show();
+                                        break;
                                     }
-
                                 }
                             }
                         }
                     });
+                    parent.addView(signUpButton);
 
-                    parent.addView(btn1);
                 } else if (componentModel.getType().equals("text")) {
+                    TextView alreadyAccountTextView = new TextView(getActivity());
+                    alreadyAccountTextView.setLayoutParams(matchWidthParams);
+                    alreadyAccountTextView.setText(componentModel.getLabel());
+                    alreadyAccountTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+                    alreadyAccountTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+                    alreadyAccountTextView.setTextColor(Color.parseColor("#1f9bde"));
 
-                    LinearLayout.LayoutParams alreadyAccountLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0);
-                    LinearLayout alreadyAccountLayout = new LinearLayout(getActivity());
-                    alreadyAccountLayout.setLayoutParams(alreadyAccountLayoutParams);
-                    alreadyAccountLayout.setOrientation(LinearLayout.VERTICAL);
-                    alreadyAccountLayoutParams.weight = 1;
-                    alreadyAccountLayoutParams.gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
-
-
-                    TextView tv1 = new TextView(getActivity());
-                    tv1.setLayoutParams(matchWidthParams);
-                    tv1.setText(componentModel.getLabel());
-                    tv1.setGravity(Gravity.CENTER_HORIZONTAL);
-                    tv1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
-                    tv1.setTextColor(Color.parseColor("#1f9bde"));
-
-                    // tv1.setTypeface(et1.getTypeface(), Typeface.BOLD);
-
-
-                    RelativeLayout relative = new RelativeLayout(getActivity());
-                    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(AppBarLayout.LayoutParams.WRAP_CONTENT, AppBarLayout.LayoutParams.WRAP_CONTENT);
+                    RelativeLayout alreadyAccountLayout = new RelativeLayout(getActivity());
+                    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
                     params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
                     params.addRule(RelativeLayout.CENTER_HORIZONTAL);
-                    relative.setGravity(Gravity.BOTTOM | Gravity.CENTER);
-                    relative.addView(tv1, params);
-                    parent.addView(relative, new AppBarLayout.LayoutParams(AppBarLayout.LayoutParams.MATCH_PARENT, AppBarLayout.LayoutParams.WRAP_CONTENT));
+                    alreadyAccountLayout.setGravity(Gravity.BOTTOM | Gravity.CENTER);
+                    alreadyAccountLayout.addView(alreadyAccountTextView, params);
+                    parent.addView(alreadyAccountLayout, new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
                 }
             }
         }
@@ -169,10 +135,7 @@ public class SignUpFragment extends Fragment {
             isValid = true;
         }
         return isValid;
-
     }
-
-
 }
 
 
