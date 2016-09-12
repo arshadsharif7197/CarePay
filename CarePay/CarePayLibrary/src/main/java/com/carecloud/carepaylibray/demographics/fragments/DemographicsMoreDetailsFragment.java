50cfe3d5ba5d81fb27ebd0cfc82f3e77aff7c6b2
grbhargav-carecloud <RB-19@Rakeshs-MacBook-Pro.local>
2016-09-12 17:13:11 -0400

    SHMRK-265 Made changes to Demographics

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/demographics/fragments/DemographicsMoreDetailsFragment.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/demographics/fragments/DemographicsMoreDetailsFragment.java
@@ -9,12 +9,18 @@ import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.carecloud.carepaylibrary.R;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.carecloud.carepaylibray.utils.Utility.setGothamRoundedMediumTypeface;
+import static com.carecloud.carepaylibray.utils.Utility.setProximaNovaRegularTypeface;
+import static com.carecloud.carepaylibray.utils.Utility.setProximaNovaSemiboldTypeface;
 
 /**
  * Created by lsoco_user on 9/2/2016.
@@ -31,72 +37,24 @@ public class DemographicsMoreDetailsFragment extends Fragment implements View.On
         view = inflater.inflate(R.layout.fragment_demographics_moredetails, container, false);
 
         getUpdateItemList = getResources().getStringArray(R.array.UpdatesMode);
+        setTypefaces(view);
         return view;
     }
 
 
     @Override
     public void onClick(View view) {
-        if (view == wantUpdateSwitch) {
-            switchActionForWantUpdateSwitch(wantUpdateSwitch.isChecked());
-        }
+
     }
 
-    private void switchActionForWantUpdateSwitch(boolean checked) {
-        if (checked) {
-            selectedUpdateItemList = new ArrayList<String>();
-            showAlertDialog();
-        } else {
+    private void setTypefaces(View view) {
+        setGothamRoundedMediumTypeface(getActivity(), (TextView) view.findViewById(R.id.moreDetailsHeading));
+        setProximaNovaRegularTypeface(getActivity(), (TextView) view.findViewById(R.id.moreDetailsSubHeading));
+        setGothamRoundedMediumTypeface(getActivity(),(Button)view.findViewById(R.id.goToCarePayButton));
 
-        }
+        setProximaNovaRegularTypeface(getActivity(), (TextView) view.findViewById(R.id.placeHolderIconTextViewId));
     }
 
-    private void showAlertDialog() {
-        final CharSequence[] dialogList = getUpdateItemList;
-        final AlertDialog.Builder builderDialog = new AlertDialog.Builder(getActivity());
-        builderDialog.setTitle("Select Updates");
-        int count = dialogList.length;
-        final boolean[] is_checked = new boolean[count];
 
-        builderDialog.setMultiChoiceItems(dialogList, is_checked,
-                new DialogInterface.OnMultiChoiceClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton, boolean isChecked) {
-                        if (isChecked) {
-                            selectedUpdateItemList.add(getUpdateItemList[whichButton]);
-                        } else {
-                            String selectedLable = getUpdateItemList[whichButton];
-                            for (int index = 0; index < selectedUpdateItemList.size(); index++) {
-                                if (selectedLable.equalsIgnoreCase(selectedUpdateItemList.get(index))) {
-                                    selectedUpdateItemList.remove(index);
-                                }
-                            }
-                        }
-                    }
-                });
-
-        builderDialog.setPositiveButton("Select",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        if (selectedUpdateItemList.size() <= 0)
-                            wantUpdateSwitch.setChecked(false);
-                        Toast.makeText(getActivity(), selectedUpdateItemList.toString(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-        builderDialog.setNegativeButton("Cancel",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        selectedUpdateItemList.clear();
-                        wantUpdateSwitch.setChecked(false);
-                        Toast.makeText(getActivity(), "" + selectedUpdateItemList.toString(), Toast.LENGTH_SHORT).show();
-
-
-                    }
-                });
-        AlertDialog alert = builderDialog.create();
-        alert.show();
-    }
 
 }
