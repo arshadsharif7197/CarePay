16ee265fae290d1fd79de0491ed2dafff6828b30
Md Jahirul Islam Bhuiyan <md.jahirul.islam.bhuiyan.2014@gmail.com>
2016-10-07 15:44:53 -0400

    Change Edittext name

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/customcomponents/CustomEditTextBase.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/customcomponents/CustomEditTextBase.java
@@ -1,14 +1,11 @@
 package com.carecloud.carepaylibray.customcomponents;
 
-/**
- * Created by harshal_patil on 9/26/2016.
- */
-
 import android.content.Context;
 import android.graphics.Typeface;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.content.ContextCompat;
+import android.text.InputType;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,45 +16,44 @@ import com.carecloud.carepaylibray.utils.StringUtil;
 
 import org.apache.commons.lang3.text.WordUtils;
 
-public class CustomInputEditText extends TextInputLayout {
+/**
+ * Created by Jahirul Bhuiyan on 10/7/2016.
+ */
+
+public abstract class CustomEditTextBase extends TextInputLayout {
     Context context;
     EditText input;
 
-    public CustomInputEditText(Context context) {
+    public CustomEditTextBase(Context context) {
         super(context);
         this.context=context;
         addViews();
     }
 
-    public CustomInputEditText(Context context, AttributeSet attrs) {
+    public CustomEditTextBase(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.context=context;
         addViews();
     }
 
-    public CustomInputEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+    public CustomEditTextBase(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.context=context;
         addViews();
 
     }
-    private void addViews() {
-        TextInputLayout.LayoutParams params=new AppBarLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+    public void addViews() {
+        TextInputLayout.LayoutParams params=new AppBarLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
         input= new EditText(context);
         input.setLayoutParams(params);
-        input.setTextColor(ContextCompat.getColor(context, R.color.blue_cerulian));
-        //input.setImeOptions(EditorInfo.IME_ACTION_NEXT);
-        this.setHintTextAppearance(R.style.TextAppearence_App_TextInputLayout);
-        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/proximanova_semibold.otf");
-        setTypeface(tf);
         input.setOnFocusChangeListener(new OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
-                CustomInputEditText.super.getOnFocusChangeListener().onFocusChange(view,b);
+                CustomEditTextBase.super.getOnFocusChangeListener().onFocusChange(view,b);
                 if(b) {
-                    CustomInputEditText.this.setHint( CustomInputEditText.this.getHint().toString().toUpperCase());// WordUtils.capitalize(CustomInputEditText.this.getHint().toString()));
+                    CustomEditTextBase.this.setHint( CustomEditTextBase.this.getHint().toString().toUpperCase());// WordUtils.capitalize(CustomInputEditText.this.getHint().toString()));
                 }else if(!b && StringUtil.isNullOrEmpty(input.getText().toString())){
-                    CustomInputEditText.this.setHint( WordUtils.capitalizeFully(CustomInputEditText.this.getHint().toString()));
+                    CustomEditTextBase.this.setHint( WordUtils.capitalizeFully(CustomEditTextBase.this.getHint().toString()));
                 }
             }
 
