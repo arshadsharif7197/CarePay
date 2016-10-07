fda6a2c7d189ebe3a6cd533310f6edac59a89c3d
Md Jahirul Islam Bhuiyan <md.jahirul.islam.bhuiyan.2014@gmail.com>
2016-10-07 14:07:21 -0400

    SHMRK-471
    1. Remove unused library from gradle
    2. Remove duplicate library from gradle
    3. Add SystemUiVisibility
    4. Add custom input focus functionality
    
    https://jira.carecloud.com/browse/SHMRK-471

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/customcomponents/CustomInputEditText.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/customcomponents/CustomInputEditText.java
@@ -10,10 +10,14 @@ import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
 import com.carecloud.carepaylibrary.R;
+import com.carecloud.carepaylibray.utils.StringUtil;
+
+import org.apache.commons.lang3.text.WordUtils;
 
 public class CustomInputEditText extends TextInputLayout {
     Context context;
@@ -46,7 +50,18 @@ public class CustomInputEditText extends TextInputLayout {
         this.setHintTextAppearance(R.style.TextAppearence_App_TextInputLayout);
         Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/proximanova_semibold.otf");
         setTypeface(tf);
+        input.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                CustomInputEditText.super.getOnFocusChangeListener().onFocusChange(view,b);
+                if(b) {
+                    CustomInputEditText.this.setHint( CustomInputEditText.this.getHint().toString().toUpperCase());// WordUtils.capitalize(CustomInputEditText.this.getHint().toString()));
+                }else if(!b && StringUtil.isNullOrEmpty(input.getText().toString())){
+                    CustomInputEditText.this.setHint( WordUtils.capitalizeFully(CustomInputEditText.this.getHint().toString()));
+                }
+            }
 
+        });
         this.addView(input);
     }
     public void changeInputType(int inputType){
