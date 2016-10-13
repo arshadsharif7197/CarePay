9470d812ba8986783da5b754fddf9db2c43db1eb
jahirul-carecloud <c-jahirul_bhuiyan@persistent.com>
2016-10-13 15:18:55 -0400

    SHMRK-602
    1. Change Switch to if because of there might not be many case.
    
    https://jira.carecloud.com/browse/SHMRK-602

--- CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/SplashActivity.java
+++ CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/SplashActivity.java
@@ -5,13 +5,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-
 import com.carecloud.carepaylibray.activities.LibraryMainActivity;
-import com.carecloud.carepaylibray.appointments.activities.AppointmentsActivity;
+
+/**
+ * Created by Jahirul Bhuiyan on 10/13/2016.
+ * This is the Launcher activity for the patient app
+ * Applied logic:
+ * If application language not yet selected navigate to language selection screen
+ * if user authentication not found navigate to login screen
+ * else navigate to appointment screen
+ * */
 
 public class SplashActivity extends Activity {
+
     private static final int STOPSPLASH = 0;
     private static final long SPLASHTIME = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +34,11 @@ public class SplashActivity extends Activity {
     private Handler splashHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case STOPSPLASH:
-                    Intent intent = new Intent(SplashActivity.this, LibraryMainActivity.class);
-                    startActivity(intent);
-                    SplashActivity.this.finish();
-                    break;
+            // TODO: Need to apply navigation logic
+            if(msg.what==STOPSPLASH){
+                Intent intent = new Intent(SplashActivity.this, LibraryMainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
             }
             super.handleMessage(msg);
         }
