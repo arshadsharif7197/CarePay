961c0a58b76b55992e54c210f315058431ca299b
jahirul-carecloud <c-jahirul_bhuiyan@persistent.com>
2016-10-17 18:52:27 -0400

    SHMRK-473
    1. Remove all files from CarePayClover project to CarePayPractice library and Clover project based on the features
    2. Delete CarePayClover project.
    3. Delete CarePayComponentLibrary because we will use common library for any custom component.
    4. Add all resource that need for practice home screen
    
    https://jira.carecloud.com/browse/SHMRK-473

---
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/checkin/CheckInActivity.java
@@ -0,0 +1,70 @@
+package com.carecloud.carepay.practice.library.checkin;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.carecloud.carepay.practice.library.R;
+import com.carecloud.carepay.practice.library.checkin.adapters.CheckedInAdapter;
+import com.carecloud.carepay.service.library.BaseServiceGenerator;
+import com.carecloud.carepaylibray.appointments.models.AppointmentsResultModel;
+import com.carecloud.carepaylibray.appointments.services.AppointmentService;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class CheckInActivity extends AppCompatActivity {
+
+    private RecyclerView appointmentsRecyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_in);
+        appointmentsRecyclerView = (RecyclerView) findViewById(com.carecloud.carepaylibrary.R.id.appointments_recycler_view);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        findViewById(R.id.goBackTextview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getDemographicInformation();
+    }
+
+    private void getDemographicInformation() {
+        AppointmentService apptService = (new BaseServiceGenerator(this)).createServicePractice(AppointmentService.class); //, String token, String searchString
+        Call<AppointmentsResultModel> call = apptService.fetchCheckedInAppointments();
+        call.enqueue(new Callback<AppointmentsResultModel>() {
+            @Override
+            public void onResponse(Call<AppointmentsResultModel> call, Response<AppointmentsResultModel> response) {
+
+                if(response.code()==200 && response.body().getPayload()!=null && response.body().getPayload().getAppointments()!=null) {
+                    CheckedInAdapter checkedInAdapter = new CheckedInAdapter(CheckInActivity.this, new ArrayList<>(response.body().getPayload().getAppointments()));
+                    appointmentsRecyclerView.setLayoutManager(new LinearLayoutManager(CheckInActivity.this));
+                    appointmentsRecyclerView.setAdapter(checkedInAdapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AppointmentsResultModel> call, Throwable throwable) {
+
+            }
+        });
+    }
+}
---
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/homescreen/CloverMainActivity.java
@@ -0,0 +1,88 @@
+package com.carecloud.carepay.practice.library.homescreen;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.carecloud.carepay.practice.library.R;
+import com.carecloud.carepay.practice.library.checkin.CheckInActivity;
+import com.carecloud.carepay.service.library.BaseServiceGenerator;
+import com.carecloud.carepaylibray.appointments.models.AppointmentsResultModel;
+import com.carecloud.carepaylibray.appointments.services.AppointmentService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class CloverMainActivity extends AppCompatActivity {
+public static int count;
+TextView checkedInCounterTextview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setSystemUiVisibility();
+        setContentView(R.layout.activity_main_clover);
+        checkedInCounterTextview= (TextView) findViewById(R.id.checkedInCounterTextview);
+        getDemographicInformation();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        ((TextView) findViewById(R.id.checkinTextView)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent checkedInIntent = new Intent(CloverMainActivity.this, CheckInActivity.class);
+                checkedInIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(checkedInIntent);
+            }
+        });
+        registerReceiver(newCheckedInReceiver, new IntentFilter("NEW_CHECKEDIN_NOTIFICATION"));
+    }
+
+    public void setSystemUiVisibility() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    BroadcastReceiver newCheckedInReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            count=Integer.parseInt(checkedInCounterTextview.getText().toString())+1;
+            checkedInCounterTextview.setText(String.valueOf( count));
+        }
+    };
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(newCheckedInReceiver);
+    }
+
+    private void getDemographicInformation() {
+        AppointmentService apptService = (new BaseServiceGenerator(this)).createServicePractice(AppointmentService.class); //, String token, String searchString
+        Call<AppointmentsResultModel> call = apptService.fetchCheckedInAppointments();
+        call.enqueue(new Callback<AppointmentsResultModel>() {
+            @Override
+            public void onResponse(Call<AppointmentsResultModel> call, Response<AppointmentsResultModel> response) {
+
+                if(response.code()==200 && response.body().getPayload()!=null && response.body().getPayload().getAppointments()!=null) {
+                    checkedInCounterTextview.setText(String.valueOf( response.body().getPayload().getAppointments().size()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AppointmentsResultModel> call, Throwable t) {
+
+            }
+        });
+    }
+
+}
