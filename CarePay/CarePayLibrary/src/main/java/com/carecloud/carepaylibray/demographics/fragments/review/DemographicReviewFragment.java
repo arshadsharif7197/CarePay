da44072dcc1547a61ce734c886baeca8d5ecf53d
rahulg-CareCloud <c-rahul_girase@persistent.com>
2016-10-20 19:27:45 -0400

    Code Climate Change optimizing imports

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/demographics/fragments/review/DemographicReviewFragment.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/demographics/fragments/review/DemographicReviewFragment.java
@@ -43,15 +43,17 @@ import com.carecloud.carepaylibray.utils.DateUtil;
 import com.carecloud.carepaylibray.utils.StringUtil;
 import com.carecloud.carepaylibray.utils.SystemUtil;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static com.carecloud.carepaylibray.utils.SystemUtil.setGothamRoundedMediumTypeface;
 import static com.carecloud.carepaylibray.utils.SystemUtil.setProximaNovaExtraboldTypefaceInput;
 import static com.carecloud.carepaylibray.utils.SystemUtil.setProximaNovaRegularTypeface;
 import static com.carecloud.carepaylibray.utils.SystemUtil.setProximaNovaRegularTypefaceLayout;
 import static com.carecloud.carepaylibray.utils.SystemUtil.setProximaNovaSemiboldTypeface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
