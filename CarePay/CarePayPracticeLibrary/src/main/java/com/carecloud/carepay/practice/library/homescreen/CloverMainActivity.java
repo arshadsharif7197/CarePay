448b8fae4a3f6a181365a9d0f215cd44db429262
rahulg-CareCloud <c-rahul_girase@persistent.com>
2016-10-20 21:58:57 -0400

    Code Fix for Clover Main Activity

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/homescreen/CloverMainActivity.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/homescreen/CloverMainActivity.java
@@ -31,7 +31,7 @@ public class CloverMainActivity extends AppCompatActivity {
         setSystemUiVisibility();
         setContentView(R.layout.activity_main_clover);
         checkedInCounterTextview = (TextView) findViewById(R.id.checkedInCounterTextview);
-        getDemographicInformation();
+       // getDemographicInformation();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         ((TextView) findViewById(R.id.checkinTextView)).setOnClickListener(new View.OnClickListener() {
@@ -69,7 +69,7 @@ public class CloverMainActivity extends AppCompatActivity {
         unregisterReceiver(newCheckedInReceiver);
     }
 
-    private void getDemographicInformation() {
+    /*private void getDemographicInformation() {
         AppointmentService apptService = (new BaseServiceGenerator(this)).createService(AppointmentService.class); //, String token, String searchString
         Call<AppointmentsResultModel> call = apptService.fetchCheckedInAppointments();
         call.enqueue(new Callback<AppointmentsResultModel>() {
@@ -86,6 +86,6 @@ public class CloverMainActivity extends AppCompatActivity {
 
             }
         });
-    }
+    }*/
 
 }
