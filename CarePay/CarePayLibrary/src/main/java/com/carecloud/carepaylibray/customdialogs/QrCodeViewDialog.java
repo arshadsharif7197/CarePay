4d5c5b7ada360c880c98a8b3641f335da980d278
Arpit Jain <arpit_jain1@persistent.co.in>
2016-10-24 18:35:53 +0530

    SHMRK-691-QR-code-pop-up
     - Code implemented to show QR code on click of check in at office button

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/customdialogs/QrCodeViewDialog.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/customdialogs/QrCodeViewDialog.java
@@ -2,22 +2,33 @@ package com.carecloud.carepaylibray.customdialogs;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.carecloud.carepaylibrary.R;
+import com.carecloud.carepaylibray.appointments.models.AppointmentDTO;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 public class QrCodeViewDialog extends Dialog implements View.OnClickListener {
 
     private Context context;
+    private AppointmentDTO appointmentDTO;
+    public final static int WIDTH = 500;
 
-    public QrCodeViewDialog(Context context) {
+    public QrCodeViewDialog(Context context, AppointmentDTO appointmentDTO) {
         super(context);
         this.context = context;
+        this.appointmentDTO = appointmentDTO;
     }
 
     @Override
@@ -31,8 +42,50 @@ public class QrCodeViewDialog extends Dialog implements View.OnClickListener {
         params.height = LinearLayout.LayoutParams.WRAP_CONTENT;
         params.width = (int) (context.getResources().getDisplayMetrics().widthPixels * 0.90);
         getWindow().setAttributes(params);
+
+        ImageView qrCodeImageView = (ImageView) findViewById(R.id.qrCodeImageView);
+        try {
+            Bitmap bitmap = encodeAsBitmap(appointmentDTO.getPayload().getId());
+            qrCodeImageView.setImageBitmap(bitmap);
+        } catch (WriterException ex) {
+            ex.printStackTrace();
+        }
+
         (findViewById(R.id.qrcodeCancelButton)).setOnClickListener(this);
         (findViewById(R.id.dialogQRHeaderTextView)).setOnClickListener(this);
+    }
+
+    /**
+     * @param convertToQRCode string which needs to be converted to QR code
+     * @return
+     * @throws WriterException
+     */
+    Bitmap encodeAsBitmap(String convertToQRCode) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(convertToQRCode, BarcodeFormat.QR_CODE, WIDTH,
+                    WIDTH, null);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        int width = result.getWidth();
+        int height = result.getHeight();
+        int[] pixels = new int[width * height];
+        int heightCounter = 0;
+        while (heightCounter < height) {
+            int offset = heightCounter * width;
+            for (int widthCounter = 0; widthCounter < width; widthCounter++)
+                pixels[offset + widthCounter] = result.get(widthCounter, heightCounter) ?
+                        ContextCompat.getColor(context, R.color.black)
+                        : ContextCompat.getColor(context, R.color.white);
+
+            heightCounter++;
+        }
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, WIDTH, 0, 0, width, height);
+        return bitmap;
     }
 
     @Override
