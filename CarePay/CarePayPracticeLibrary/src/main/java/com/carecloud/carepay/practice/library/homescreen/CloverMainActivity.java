9a07276da89ef16b067837985c6a4052fe91415f
liviu-carecloud <liviu_socolovici@persistent.com>
2016-11-01 17:44:18 -0400

    SHMRK-788 - added load labels from back-end functionality

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/homescreen/CloverMainActivity.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/homescreen/CloverMainActivity.java
@@ -22,10 +22,14 @@ import com.carecloud.carepay.practice.library.base.NavigationHelper;
 import com.carecloud.carepay.practice.library.customdialog.ChangeModeDialog;
 import com.carecloud.carepay.practice.library.homescreen.dtos.HomeScreenAppointmentCountsDTO;
 import com.carecloud.carepay.practice.library.homescreen.dtos.HomeScreenDTO;
+import com.carecloud.carepay.practice.library.homescreen.dtos.HomeScreenLabelDTO;
+import com.carecloud.carepay.practice.library.homescreen.dtos.HomeScreenMetadataDTO;
 import com.carecloud.carepay.practice.library.patientmode.PatientModeSplashActivity;
 import com.carecloud.carepay.service.library.WorkflowServiceCallback;
 import com.carecloud.carepay.service.library.WorkflowServiceHelper;
 import com.carecloud.carepay.service.library.dtos.WorkflowDTO;
+import com.carecloud.carepaylibray.constants.CarePayConstants;
+import com.carecloud.carepaylibray.utils.StringUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +45,16 @@ public class CloverMainActivity extends BasePracticeActivity implements View.OnC
     private       Spinner       homeSpinner;
     private       LinearLayout  homeCheckinLl;
     private       LinearLayout  homeAlertLinearLl;
+    private       TextView      homeQueueLabel;
+    private       TextView      homeAlertsLabel;
+    private       TextView      homeCheckinLabel;
+    private       TextView      homePaymentsLabel;
+    private       TextView      homeAppointmentsLabel;
+    private       TextView      homeCheckoutLabel;
+    private       TextView      homeShopLabel;
+
+    private List<String> modeSwitchOptions = new ArrayList<>();
+    private List<String> langs             = new ArrayList<>();
 
     public enum HomeScreenMode {
         PATIENT_HOME, PRACTICE_HOME
@@ -58,22 +72,16 @@ public class CloverMainActivity extends BasePracticeActivity implements View.OnC
         HomeScreenMode homeScreenMode = HomeScreenMode.valueOf(homeScreenDTO.getState().toUpperCase());
         setContentView(R.layout.activity_main_clover);
 
-        // init UI handlers
-        homeCheckinLl = (LinearLayout) findViewById(R.id.homeQueueLayout);
-        homeAlertLinearLl = (LinearLayout) findViewById(R.id.homeAlertLayout);
-        homeLockImageView = (ImageView) findViewById(R.id.homeLockIcon);
-        checkedInCounterTextview = (TextView) findViewById(R.id.checkedInCounterTextview);
-        alertTextView = (TextView) findViewById(R.id.alertTextView);
-        modeSwitchImageView = (ImageView) findViewById(R.id.homeModeSwitchClickable);
-        modeSwitchImageView.setOnClickListener(this);
+        // init UI fields
+        initUIFields();
+
+        createChangeModeDialog();
 
         populateWithLabels();
 
+        modeSwitchImageView.setOnClickListener(this);
+
         // set the spinner
-        homeSpinner = (Spinner) findViewById(R.id.homePatientLangSpinner);
-        List<String> langs = new ArrayList<>();
-        langs.add("EN");
-        langs.add("ES");
         ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(this, R.layout.home_spinner_item, langs);
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         homeSpinner.setAdapter(spinnerArrayAdapter);
@@ -98,8 +106,40 @@ public class CloverMainActivity extends BasePracticeActivity implements View.OnC
         registerReceiver(newCheckedInReceiver, new IntentFilter("NEW_CHECKEDIN_NOTIFICATION"));
     }
 
-    private void populateWithLabels() {
+    private void initUIFields() {
+        homeCheckinLl = (LinearLayout) findViewById(R.id.homeQueueLayout);
+        homeAlertLinearLl = (LinearLayout) findViewById(R.id.homeAlertLayout);
+        homeLockImageView = (ImageView) findViewById(R.id.homeLockIcon);
+        checkedInCounterTextview = (TextView) findViewById(R.id.checkedInCounterTextview);
+        alertTextView = (TextView) findViewById(R.id.alertTextView);
+        modeSwitchImageView = (ImageView) findViewById(R.id.homeModeSwitchClickable);
+        homeSpinner = (Spinner) findViewById(R.id.homePatientLangSpinner);
+        homeQueueLabel = (TextView) findViewById(R.id.queueTitleTextView);
+        homeAlertsLabel = (TextView) findViewById(R.id.alaertTitleTextView);
+        homeCheckinLabel = (TextView) findViewById(R.id.homeCheckinLabel);
+        homePaymentsLabel = (TextView) findViewById(R.id.homePaymentsLabel);
+        homeAppointmentsLabel = (TextView) findViewById(R.id.homeAppointmentsLabel);
+        homeCheckoutLabel = (TextView) findViewById(R.id.homeCheckoutLabel);
+        homeShopLabel = (TextView) findViewById(R.id.homeShopLabel);
+    }
 
+    private void populateWithLabels() {
+        HomeScreenMetadataDTO metadataDTO = homeScreenDTO.getMetadata();
+
+        HomeScreenLabelDTO labels = metadataDTO.getLabel();
+
+        homeQueueLabel.setText(labels == null ? CarePayConstants.NOT_DEFINED : StringUtil.getLabelForView(labels.getCheckinginNotifications()));
+        homeAlertsLabel.setText(labels == null ? CarePayConstants.NOT_DEFINED : StringUtil.getLabelForView(labels.getAlerts()));
+        homeCheckinLabel.setText(labels == null ? CarePayConstants.NOT_DEFINED : StringUtil.getLabelForView(labels.getCheckinButton()));
+        homePaymentsLabel.setText(labels == null ? CarePayConstants.NOT_DEFINED : StringUtil.getLabelForView(labels.getPaymentsButton()));
+        homeAppointmentsLabel.setText(labels == null ? CarePayConstants.NOT_DEFINED : StringUtil.getLabelForView(labels.getAppointmentsButton()));
+        homeCheckoutLabel.setText(labels == null ? CarePayConstants.NOT_DEFINED : StringUtil.getLabelForView(labels.getCheckoutButton()));
+        homeShopLabel.setText(labels == null ? CarePayConstants.NOT_DEFINED : StringUtil.getLabelForView(labels.getShopButton()));
+
+        // load mode switch options
+        modeSwitchOptions.clear();
+        modeSwitchOptions.add(labels == null ? CarePayConstants.NOT_DEFINED : StringUtil.getLabelForView(labels.getPatientModeLabel()));
+        modeSwitchOptions.add(labels == null ? CarePayConstants.NOT_DEFINED : StringUtil.getLabelForView(labels.getLogoutLabel()));
     }
 
     private void changeScreenMode(HomeScreenMode homeScreenMode) {
@@ -149,37 +189,13 @@ public class CloverMainActivity extends BasePracticeActivity implements View.OnC
         int viewId = view.getId();
 
         if (viewId == R.id.homeCheckinClickable) {
-            /*Intent checkedInIntent = new Intent(CloverMainActivity.this, CheckInActivity.class);
-            checkedInIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(checkedInIntent);*/
-            if (homeScreenDTO != null && homeScreenDTO.getMetadata() != null && homeScreenDTO.getMetadata().getTransitions() != null) {
-                WorkflowServiceHelper.getInstance().execute(homeScreenDTO.getMetadata().getTransitions().getPracticeCheckin(), checkInCallback);
-            }
+            WorkflowServiceHelper.getInstance().execute(homeScreenDTO.getMetadata().getTransitions().getPracticeCheckin(), checkInCallback);
         } else if (viewId == R.id.homeModeSwitchClickable) {
-
-            ChangeModeDialog changeModeDialog = new ChangeModeDialog(this, new ChangeModeDialog.PatientModeClickListener() {
-
-                @Override
-                public void onPatientModeSelected() {
-                    Intent appointmentIntent = new Intent(CloverMainActivity.this, PatientModeSplashActivity.class);
-                    appointmentIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(appointmentIntent);
-                    Toast.makeText(CloverMainActivity.this, "Patient Mode selected...", Toast.LENGTH_SHORT).show();
-                }
-            }, new ChangeModeDialog.LogoutClickListener() {
-
-                @Override
-                public void onLogoutSelected() {
-                    Toast.makeText(CloverMainActivity.this, "Logout selected...", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-            changeModeDialog.show();
+            createChangeModeDialog().show();
         } else if (viewId == R.id.homeAppointmentsClickable) {
             Intent appointmentIntent = new Intent(CloverMainActivity.this, AppointmentsActivity.class);
             appointmentIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(appointmentIntent);
-
         }
     }
 
@@ -200,19 +216,20 @@ public class CloverMainActivity extends BasePracticeActivity implements View.OnC
         }
     };
 
-    /*private void getDemographicInformation() {
-        AppointmentService apptService = (new BaseServiceGenerator(this)).createService(AppointmentService.class); //, String token, String searchString
-        Call<AppointmentsResultModel> call = apptService.fetchCheckedInAppointments();
-        call.enqueue(new Callback<AppointmentsResultModel>() {
+    private ChangeModeDialog createChangeModeDialog() {
+        return new ChangeModeDialog(this, new ChangeModeDialog.PatientModeClickListener() {
             @Override
-            public void onResponse(Call<AppointmentsResultModel> call, Response<AppointmentsResultModel> response) {
-                if (response.code() == 200 && response.body().getPayload() != null && response.body().getPayload().getAppointments() != null) {
-                    checkedInCounterTextview.setText(String.valueOf(response.body().getPayload().getAppointments().size()));
-                }
+            public void onPatientModeSelected() {
+                Intent appointmentIntent = new Intent(CloverMainActivity.this, PatientModeSplashActivity.class);
+                appointmentIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(appointmentIntent);
+                Toast.makeText(CloverMainActivity.this, "Patient Mode selected...", Toast.LENGTH_SHORT).show();
             }
+        }, new ChangeModeDialog.LogoutClickListener() {
             @Override
-            public void onFailure(Call<AppointmentsResultModel> call, Throwable throwable) {
+            public void onLogoutSelected() {
+                Toast.makeText(CloverMainActivity.this, "Logout selected...", Toast.LENGTH_SHORT).show();
             }
-        });
-    }*/
+        }, modeSwitchOptions);
+    }
 }
