2f13b38d9f9f254096002ae48886512c8aa26e67
liviu-carecloud <liviu_socolovici@persistent.com>
2016-11-09 17:17:38 -0500

    SHMRK-883 - added dynamic transition from patient splash to patient  home

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/patientmode/dtos/PatientModeTransitionsDTO.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/patientmode/dtos/PatientModeTransitionsDTO.java
@@ -4,6 +4,7 @@ package com.carecloud.carepay.practice.library.patientmode.dtos;
  * Created by Rahul on 10/27/16.
  */
 
+import com.carecloud.carepay.service.library.dtos.TransitionDTO;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -11,20 +12,19 @@ public class PatientModeTransitionsDTO {
 
     @SerializedName("start")
     @Expose
-    private PatientModesStartDTO start;
+    private TransitionDTO start;
 
     /**
      * @return The start
      */
-    public PatientModesStartDTO getStart() {
+    public TransitionDTO getStart() {
         return start;
     }
 
     /**
      * @param start The start
      */
-    public void setStart(PatientModesStartDTO start) {
+    public void setStart(TransitionDTO start) {
         this.start = start;
     }
-
 }
