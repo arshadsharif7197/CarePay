6228c917f5259cdc445614b5e0f9c803319f43c5
liviu-carecloud <liviu_socolovici@persistent.com>
2016-11-13 17:31:19 -0500

    SHMRK-888 - fixed CodeClimate(3)

--- CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/consentforms/ConsentActivity.java
+++ CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/consentforms/ConsentActivity.java
@@ -32,8 +32,6 @@ import static com.carecloud.carepaylibray.keyboard.KeyboardHolderActivity.LOG_TA
 import static com.carecloud.carepaylibray.utils.SystemUtil.setGothamRoundedMediumTypeface;
 
 
-
-
 public class ConsentActivity extends BasePatientActivity implements IFragmentCallback {
 
 
