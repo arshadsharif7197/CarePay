a7fb3531ab3ff0cd7b1ad131bc3b5aae9916a2d9
jahirul-carecloud <c-jahirul_bhuiyan@persistent.com>
2016-11-14 18:38:22 -0500

    SHMRK-886
    Implement separate QR scanner for clover and tablet
    Clover activity use clover SDK
    Tablet use default implementation provided by Xing
    Note: I will continue integration with the patient app in my next PR.
    
    https://jira.carecloud.com/browse/SHMRK-886

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/checkin/activities/HowToCheckInActivity.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/checkin/activities/HowToCheckInActivity.java
@@ -1,21 +1,27 @@
 package com.carecloud.carepay.practice.library.checkin.activities;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.carecloud.carepay.practice.library.R;
 import com.carecloud.carepay.practice.library.base.BasePracticeActivity;
 import com.carecloud.carepay.practice.library.base.PracticeNavigationHelper;
 import com.carecloud.carepay.practice.library.homescreen.CloverMainActivity;
-import com.carecloud.carepay.practice.library.signin.SigninActivity;
 import com.carecloud.carepay.practice.library.signin.dtos.SigninPatientModeDTO;
 import com.carecloud.carepay.practice.library.signin.dtos.SigninPatientModeLabelsDTO;
 import com.carecloud.carepay.service.library.WorkflowServiceCallback;
 import com.carecloud.carepay.service.library.WorkflowServiceHelper;
+import com.carecloud.carepay.service.library.constants.HttpConstants;
 import com.carecloud.carepay.service.library.dtos.TransitionDTO;
 import com.carecloud.carepay.service.library.dtos.WorkflowDTO;
 import com.carecloud.carepaylibray.customcomponents.CustomGothamRoundedBookButton;
@@ -25,6 +31,12 @@ import com.carecloud.carepaylibray.utils.ApplicationPreferences;
 
 import java.util.HashMap;
 import java.util.Map;
+
+/**
+ * Modified by Jahirul Bhuiyan on 11/14/2016.
+ * This class using for scan QR code using Clover SDK
+ * Implement BroadcastReceiver for clover BarcodeResult.INTENT_ACTION
+ */
 
 public class HowToCheckInActivity extends BasePracticeActivity {
 
@@ -36,6 +48,7 @@ public class HowToCheckInActivity extends BasePracticeActivity {
     private  CustomGothamRoundedMediumButton createCarePayAccountButton;
     private CustomGothamRoundedBookButton manualSearchButton;
 
+    static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -116,6 +129,7 @@ public class HowToCheckInActivity extends BasePracticeActivity {
         @Override
         public void onClick(View view) {
             /*To implement click event for Scan QR Code*/
+            scanQR();
         }
     };
 
@@ -178,4 +192,64 @@ public class HowToCheckInActivity extends BasePracticeActivity {
 
         }
     };
+
+    /**
+     * Start QR code scanner base on the device
+     * if device is clover start CloverQRScannerActivity from clover application
+     * CloverQRScannerActivity used clover sdk for QR scanner
+     * for any other devices implement com.google.zxing.client.android.SCAN
+     */
+    public void scanQR() {
+        if(HttpConstants.getDeviceInformation().getDeviceType().equals("Clover")) {
+            Intent intent = new Intent();
+            intent.setAction("com.carecloud.carepay.practice.clover.qrscanner.CloverQRScannerActivity");
+            startActivity(intent);
+        }else {
+            try {
+                //start the scanning activity from the com.google.zxing.client.android.SCAN intent
+                Intent intent = new Intent(ACTION_SCAN);
+                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                startActivityForResult(intent, 0);
+            } catch (ActivityNotFoundException anfe) {
+                //on catch, show the download dialog
+                showDialog(HowToCheckInActivity.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
+            }
+        }
+    }
+
+    //alert dialog for downloadDialog if scanner app not found
+    private static AlertDialog showDialog(final Activity act, CharSequence title, CharSequence message, CharSequence buttonYes, CharSequence buttonNo) {
+        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
+        downloadDialog.setTitle(title);
+        downloadDialog.setMessage(message);
+        downloadDialog.setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Uri uri = Uri.parse("market://search?q=pname:" + "com.google.zxing.client.android");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                try {
+                    act.startActivity(intent);
+                } catch (ActivityNotFoundException anfe) {
+
+                }
+            }
+        });
+        downloadDialog.setNegativeButton(buttonNo, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        return downloadDialog.show();
+    }
+
+    //on ActivityResult method
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                //get the extras that are returned from the intent
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+    }
 }
