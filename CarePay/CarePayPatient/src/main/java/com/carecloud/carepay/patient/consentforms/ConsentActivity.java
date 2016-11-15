ed929f14305a639a00bc269415ed6cbd5a86980a
Sudhir Pingale <sudhir_pingale@persistent.co.in>
2016-11-15 10:40:30 +0530

    code climate changes

--- CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/consentforms/ConsentActivity.java
+++ CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/consentforms/ConsentActivity.java
@@ -30,11 +30,11 @@ import com.carecloud.carepaylibray.consentforms.models.payload.ConsentFormAppoin
 import com.carecloud.carepaylibray.consentforms.models.payload.ConsentFormPayloadDTO;
 import com.carecloud.carepaylibray.constants.CarePayConstants;
 import com.carecloud.carepaylibray.utils.DateUtil;
-import com.google.gson.Gson;
 
 import static com.carecloud.carepaylibray.keyboard.KeyboardHolderActivity.LOG_TAG;
 import static com.carecloud.carepaylibray.utils.SystemUtil.setGothamRoundedMediumTypeface;
 
+import com.google.gson.Gson;
 import java.util.Locale;
 
 public class ConsentActivity extends BasePatientActivity implements IFragmentCallback {
