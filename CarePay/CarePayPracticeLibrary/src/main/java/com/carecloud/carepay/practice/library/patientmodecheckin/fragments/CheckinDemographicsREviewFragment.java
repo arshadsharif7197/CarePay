cdbedd1e14b453ed39f249d81cc382dbd2e8b0a3
liviu-carecloud <liviu_socolovici@persistent.com>
2016-11-17 15:16:34 -0500

    SHMRK-930 - fixed missing declaration in manifest - added blue-print layouts for check in fragments

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/patientmodecheckin/fragments/CheckinDemographicsREviewFragment.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/patientmodecheckin/fragments/CheckinDemographicsREviewFragment.java
@@ -1,9 +1,24 @@
 package com.carecloud.carepay.practice.library.patientmodecheckin.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.carecloud.carepay.practice.library.R;
+
 /**
  * Created by lsoco_user on 11/17/2016.
  */
 
-public class CheckinDemographicsREviewFragment {
+public class CheckinDemographicsReviewFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_checkin_insurances_summary, container, false);
+        return view;
+    }
 }
--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/patientmodecheckin/fragments/CheckinPaymentFragment.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/patientmodecheckin/fragments/CheckinPaymentFragment.java
@@ -1,9 +1,25 @@
 package com.carecloud.carepay.practice.library.patientmodecheckin.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.carecloud.carepay.practice.library.R;
+
 /**
  * Created by lsoco_user on 11/17/2016.
  */
 
-public class CheckinPaymentFragment {
+public class CheckinPaymentFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_checkin_payment, container, false);
+        return view;
+
+    }
 
 }
