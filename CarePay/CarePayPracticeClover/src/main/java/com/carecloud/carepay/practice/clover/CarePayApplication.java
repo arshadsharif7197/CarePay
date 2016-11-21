b581554d9f3dfd898bdda87b708afc7f46e4f083
rahulg-CareCloud <c-rahul_girase@persistent.com>
2016-11-21 18:35:16 -0500

    SHMRK-563
    Cognito Bug fixes for practice app

--- CarePay/CarePayPracticeClover/src/main/java/com/carecloud/carepay/practice/clover/CarePayApplication.java
+++ CarePay/CarePayPracticeClover/src/main/java/com/carecloud/carepay/practice/clover/CarePayApplication.java
@@ -1,12 +1,18 @@
 package com.carecloud.carepay.practice.clover;
 
+import android.app.Activity;
+import android.app.Application;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.multidex.MultiDexApplication;
+import android.util.Log;
 
 import com.carecloud.carepay.practice.library.base.PracticeNavigationHelper;
+import com.carecloud.carepay.practice.library.homescreen.CloverMainActivity;
+import com.carecloud.carepay.practice.library.signin.SigninActivity;
 import com.carecloud.carepay.service.library.ApplicationPreferences;
-import com.carecloud.carepay.service.library.WorkflowServiceHelper;
+import com.carecloud.carepay.service.library.cognito.CognitoAppHelper;
 import com.carecloud.carepay.service.library.constants.ApplicationMode;
 import com.carecloud.carepay.service.library.constants.HttpConstants;
 import com.carecloud.carepay.service.library.dtos.DeviceIdentifierDTO;
@@ -15,7 +21,7 @@ import com.carecloud.carepay.service.library.dtos.DeviceIdentifierDTO;
  * Created by Jahirul Bhuiyan on 10/24/2016.
  */
 
-public class CarePayApplication extends MultiDexApplication {
+public class CarePayApplication extends MultiDexApplication implements Application.ActivityLifecycleCallbacks {
 
     @Override
     public void onCreate() {
@@ -25,10 +31,11 @@ public class CarePayApplication extends MultiDexApplication {
         ApplicationMode.getInstance().setApplicationType(ApplicationMode.ApplicationType.PRACTICE);
         PracticeNavigationHelper.initInstance(this);
         ApplicationPreferences.createPreferences(this);
+        registerActivityLifecycleCallbacks(this);
     }
 
     private void setHttpConstants() {
-        DeviceIdentifierDTO deviceIdentifierDTO=new DeviceIdentifierDTO();
+        DeviceIdentifierDTO deviceIdentifierDTO = new DeviceIdentifierDTO();
         deviceIdentifierDTO.setDeviceIdentifier(Settings.Secure.ANDROID_ID);
         deviceIdentifierDTO.setDeviceType("Clover");
         deviceIdentifierDTO.setDeviceSystemVersion(Build.VERSION.RELEASE);
@@ -37,5 +44,46 @@ public class CarePayApplication extends MultiDexApplication {
         HttpConstants.setApiStartUrl(BuildConfig.API_START_URL);
         HttpConstants.setApiStartKey(BuildConfig.X_API_KEY);
         HttpConstants.setPushNotificationWebclientUrl(BuildConfig.WEBCLIENT_URL);
+    }
+
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+        if(activity instanceof CloverMainActivity || activity instanceof SigninActivity) {
+            // log out previous user from Cognito
+            Log.v(this.getClass().getSimpleName(), "sign out Cognito");
+            CognitoAppHelper.getPool().getUser().signOut();
+            CognitoAppHelper.setUser(null);
+        }
     }
 }
