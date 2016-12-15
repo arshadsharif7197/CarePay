3c8e77f7169355b58f728c3bcbda3170309ec877
kkannan-carecloud <kkannan@carecloud.com>
2016-12-15 16:22:12 -0500

    Fixing issues that crashes the app.

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/demographics/dtos/payload/DemographicPersDetailsPayloadDTO.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/demographics/dtos/payload/DemographicPersDetailsPayloadDTO.java
@@ -3,6 +3,7 @@ package com.carecloud.carepaylibray.demographics.dtos.payload;
 import com.carecloud.carepaylibray.base.models.BasePersonModel;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import com.google.gson.internal.ObjectConstructor;
 
 /**
  * Created by Jahirul Bhuiyan on 9/26/2016.
@@ -20,8 +21,8 @@ public class DemographicPersDetailsPayloadDTO extends BasePersonModel {
     @SerializedName("ethnicity") @Expose
     private String ethnicity;
 
-    @SerializedName("preferred_language") @Expose
-    private String preferredLanguage;
+//    @SerializedName("preferred_language") @Expose
+//    private Object preferredLanguage;
 
     @SerializedName("add_unlisted_allergies") @Expose
     private String addUnlistedAllergies;
@@ -76,16 +77,16 @@ public class DemographicPersDetailsPayloadDTO extends BasePersonModel {
     /**
      * @return The preferredLanguage
      */
-    public String getPreferredLanguage() {
-        return preferredLanguage;
-    }
+//    public String getPreferredLanguage() {
+//        return preferredLanguage;
+//    }
 
     /**
      * @param preferredLanguage The preferred_language
      */
-    public void setPreferredLanguage(String preferredLanguage) {
-        this.preferredLanguage = preferredLanguage;
-    }
+//    public void setPreferredLanguage(String preferredLanguage) {
+//        this.preferredLanguage = preferredLanguage;
+//    }
 
     /**
      * @return The addUnlistedAllergies
