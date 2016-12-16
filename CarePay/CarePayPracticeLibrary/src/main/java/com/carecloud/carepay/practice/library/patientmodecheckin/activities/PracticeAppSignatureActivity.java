b1c7d56be7d2213af88e5434c12a2031f4c2ffd2
jmartinez-carecloud <jorgemfk1@gmail.com>
2016-12-15 20:14:27 -0600

    fix of issue SHMRK-1298
    fix of 2 tap on almost all forms

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/patientmodecheckin/activities/PracticeAppSignatureActivity.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/patientmodecheckin/activities/PracticeAppSignatureActivity.java
@@ -116,11 +116,12 @@ public class PracticeAppSignatureActivity extends AppCompatActivity {
         getWindow().setBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.signatureview_rounded_border));
         setContentView(R.layout.activity_signature);
       //  getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE
         );
 
         Intent intent = getIntent();
