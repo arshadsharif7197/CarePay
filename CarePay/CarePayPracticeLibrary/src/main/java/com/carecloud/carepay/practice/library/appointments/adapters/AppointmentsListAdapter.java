481d5501e3b5607cd82e586812e53eeb22485533
koustubh-carecloud <koustubh_karmalkar@persistent.co.in>
2016-12-23 16:26:38 +0530

    Fixed code climate

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/appointments/adapters/AppointmentsListAdapter.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/appointments/adapters/AppointmentsListAdapter.java
@@ -28,11 +28,11 @@ import com.carecloud.carepaylibray.utils.StringUtil;
 import com.carecloud.carepaylibray.utils.SystemUtil;
 import com.squareup.picasso.Picasso;
 
-import org.apache.commons.lang3.StringUtils;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Created by harshal_patil on 10/19/2016.
