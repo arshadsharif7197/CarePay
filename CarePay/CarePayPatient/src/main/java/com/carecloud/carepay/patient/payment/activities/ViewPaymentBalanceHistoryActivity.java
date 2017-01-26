27c522908e62772e21bb46847dc0dbcc019309ab
kkannan-carecloud <kkannan@carecloud.com>
2017-01-26 14:50:32 -0500

    Android Pay confirmation activity from PendingPayments

--- CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/payment/activities/ViewPaymentBalanceHistoryActivity.java
+++ CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/payment/activities/ViewPaymentBalanceHistoryActivity.java
@@ -1,5 +1,7 @@
 package com.carecloud.carepay.patient.payment.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -8,13 +10,19 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.carecloud.carepay.patient.R;
 import com.carecloud.carepay.patient.base.MenuPatientActivity;
+import com.carecloud.carepay.patient.payment.PaymentConstants;
+import com.carecloud.carepay.patient.payment.ResponsibilityFragment;
+import com.carecloud.carepay.patient.payment.androidpay.ConfirmationActivity;
 import com.carecloud.carepay.patient.payment.fragments.PaymentBalanceHistoryFragment;
 import com.carecloud.carepay.service.library.CarePayConstants;
 import com.carecloud.carepaylibray.payments.models.PaymentsModel;
 import com.carecloud.carepaylibray.utils.StringUtil;
+import com.google.android.gms.wallet.MaskedWallet;
+import com.google.android.gms.wallet.WalletConstants;
 import com.google.gson.Gson;
 
 /**
@@ -22,6 +30,9 @@ import com.google.gson.Gson;
  */
 
 public class ViewPaymentBalanceHistoryActivity extends MenuPatientActivity {
+
+    private PaymentsModel paymentsDTO;
+    public Bundle bundle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,7 +44,7 @@ public class ViewPaymentBalanceHistoryActivity extends MenuPatientActivity {
         navigationView = (NavigationView) findViewById(com.carecloud.carepaylibrary.R.id.nav_view_hist);
         appointmentsDrawerUserIdTextView = (TextView) navigationView.getHeaderView(0)
                 .findViewById(com.carecloud.carepaylibrary.R.id.appointmentsDrawerIdTextView);
-        PaymentsModel paymentsDTO = getConvertedDTO(PaymentsModel.class);
+        paymentsDTO = getConvertedDTO(PaymentsModel.class);
 
         practiceId = paymentsDTO.getPaymentPayload().getPatientPaymentPlans().getMetadata().getPracticeId();
         practiceMgmt = paymentsDTO.getPaymentPayload().getPatientPaymentPlans().getMetadata().getPracticeMgmt();
@@ -53,7 +64,7 @@ public class ViewPaymentBalanceHistoryActivity extends MenuPatientActivity {
         //params
         Gson gson = new Gson();
         String paymentsDTOString = gson.toJson(paymentsDTO);
-        Bundle bundle = new Bundle();
+        bundle = new Bundle();
         bundle.putString(CarePayConstants.INTAKE_BUNDLE, paymentsDTOString);
         if (paymentBalanceHistoryFragment.getArguments() != null) {
             paymentBalanceHistoryFragment.getArguments().putAll(bundle);
@@ -80,5 +91,83 @@ public class ViewPaymentBalanceHistoryActivity extends MenuPatientActivity {
             transaction.addToBackStack(fragment.getClass().getName());
         }
         transaction.commitAllowingStateLoss();
+    }
+
+    /**
+     * Invoked after the user taps the Buy With Android Pay button and the selected
+     * credit card and shipping address are confirmed. If the request succeeded,
+     * a {@link MaskedWallet} object is attached to the Intent. The Confirmation Activity is
+     * then launched, providing it with the {@link MaskedWallet} object.
+     *
+     * @param //requestCode The code that was set in the Masked Wallet Request
+     * @param //resultCode  The result of the request execution
+     * @param //data        Intent carrying the results
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // retrieve the error code, if available
+        int errorCode = -1;
+        if (data != null) {
+            errorCode = data.getIntExtra(WalletConstants.EXTRA_ERROR_CODE, -1);
+        }
+        switch (requestCode) {
+            case PaymentConstants.REQUEST_CODE_MASKED_WALLET:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        MaskedWallet maskedWallet =
+                                data.getParcelableExtra(WalletConstants.EXTRA_MASKED_WALLET);
+                        Fragment fragment = getSupportFragmentManager().findFragmentByTag(ResponsibilityFragment.class.getSimpleName());
+                        if (fragment != null) {
+                            getSupportFragmentManager().beginTransaction().remove(fragment).commit();
+                        }
+                        //setTitle("Confirmation");
+
+                        launchConfirmationPage(maskedWallet);
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                    default:
+                        handleError(errorCode);
+                        break;
+                }
+                break;
+
+            case WalletConstants.RESULT_ERROR:
+                handleError(errorCode);
+                break;
+
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
+    }
+
+    private void launchConfirmationPage(MaskedWallet maskedWallet) {
+        //setTitle("");
+        Intent intent = ConfirmationActivity.newIntent(this, maskedWallet, paymentsDTO.getPaymentPayload().getPatientBalances().get(0).getPendingRepsonsibility(), "CERT", bundle, new Gson().toJson(paymentsDTO.getPaymentsMetadata().getPaymentsLabel()));// .getPayload().get(0).getTotal(), "CERT");
+        // intent.putExtra(CarePayConstants.PAYMENT_AMOUNT_BUNDLE, paymentsDTOString);
+        startActivity(intent);
+    }
+
+
+    protected void handleError(int errorCode) {
+        switch (errorCode) {
+            case WalletConstants.ERROR_CODE_SPENDING_LIMIT_EXCEEDED:
+                Toast.makeText(this, "Way too much!!", Toast.LENGTH_LONG).show();
+                break;
+            case WalletConstants.ERROR_CODE_INVALID_PARAMETERS:
+            case WalletConstants.ERROR_CODE_AUTHENTICATION_FAILURE:
+            case WalletConstants.ERROR_CODE_BUYER_ACCOUNT_ERROR:
+            case WalletConstants.ERROR_CODE_MERCHANT_ACCOUNT_ERROR:
+            case WalletConstants.ERROR_CODE_SERVICE_UNAVAILABLE:
+            case WalletConstants.ERROR_CODE_UNSUPPORTED_API_VERSION:
+            case WalletConstants.ERROR_CODE_UNKNOWN:
+            default:
+                // unrecoverable error
+                String errorMessage = "Android Pay is unavailable" + "\n" +
+                        "Error code: " + errorCode;
+                Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
