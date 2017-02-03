00cea2237f491d694916a1bc028cb3e9225340e5
koustubh-carecloud <koustubh_karmalkar@persistent.co.in>
2017-02-03 19:13:33 +0530

    Fixed code climate

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/payments/models/PatientPaymentsDTO.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/payments/models/PatientPaymentsDTO.java
@@ -1,9 +1,9 @@
 
 package com.carecloud.carepaylibray.payments.models;
 
-import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import java.util.List;
 
 public class PatientPaymentsDTO {
 
