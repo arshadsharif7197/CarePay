629c01e6c4f936e1b0a0eea9b00cf0b11bdf0db3
lmenendez-carecloud <lmenendez@carecloud.com>
2017-02-08 09:53:27 -0500

    SHMRK-1665 Updated Loader image to use vector asset

--- CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/demographics/fragments/review/CheckinDemographicsFragment.java
+++ CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/demographics/fragments/review/CheckinDemographicsFragment.java
@@ -57,15 +57,7 @@ import com.carecloud.carepaylibray.utils.ImageCaptureHelper;
 import com.carecloud.carepaylibray.utils.ProgressDialogUtil;
 import com.carecloud.carepaylibray.utils.StringUtil;
 import com.carecloud.carepaylibray.utils.SystemUtil;
-import static com.carecloud.carepaylibray.utils.SystemUtil.hideSoftKeyboard;
-import static com.carecloud.carepaylibray.utils.SystemUtil.setGothamRoundedMediumTypeface;
-import static com.carecloud.carepaylibray.utils.SystemUtil.setProximaNovaExtraboldTypefaceInput;
-import static com.carecloud.carepaylibray.utils.SystemUtil.setProximaNovaRegularTypeface;
-import static com.carecloud.carepaylibray.utils.SystemUtil.setProximaNovaRegularTypefaceLayout;
-import static com.carecloud.carepaylibray.utils.SystemUtil.setProximaNovaSemiboldTypeface;
 import com.carecloud.carepaylibray.utils.ValidationHelper;
-
-
 import com.google.gson.Gson;
 import com.smartystreets.api.us_zipcode.City;
 import com.squareup.picasso.Picasso;
@@ -75,6 +67,13 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static com.carecloud.carepaylibray.utils.SystemUtil.hideSoftKeyboard;
+import static com.carecloud.carepaylibray.utils.SystemUtil.setGothamRoundedMediumTypeface;
+import static com.carecloud.carepaylibray.utils.SystemUtil.setProximaNovaExtraboldTypefaceInput;
+import static com.carecloud.carepaylibray.utils.SystemUtil.setProximaNovaRegularTypeface;
+import static com.carecloud.carepaylibray.utils.SystemUtil.setProximaNovaRegularTypefaceLayout;
+import static com.carecloud.carepaylibray.utils.SystemUtil.setProximaNovaSemiboldTypeface;
 
 
 public class CheckinDemographicsFragment extends DocumentScannerFragment implements View.OnClickListener {
