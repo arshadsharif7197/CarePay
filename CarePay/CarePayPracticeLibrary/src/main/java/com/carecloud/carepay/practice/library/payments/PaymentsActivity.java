3b1950ee910f4cc75e0d94065e75fa28486a534f
koustubh-carecloud <koustubh_karmalkar@persistent.co.in>
2017-02-15 17:57:55 +0530

    SHMRK-1628 (partial)

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/payments/PaymentsActivity.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/payments/PaymentsActivity.java
@@ -11,6 +11,7 @@ import com.carecloud.carepay.practice.library.checkin.filters.FilterDataDTO;
 import com.carecloud.carepay.practice.library.customcomponent.TwoColumnPatientListView;
 import com.carecloud.carepay.practice.library.customdialog.FilterDialog;
 import com.carecloud.carepay.practice.library.models.FilterModel;
+import com.carecloud.carepay.practice.library.payments.dialogs.FindPatientDialog;
 import com.carecloud.carepay.practice.library.payments.dialogs.ResponsibilityDialog;
 import com.carecloud.carepaylibray.demographicsettings.models.DemographicsSettingsPersonalDetailsPayloadDTO;
 import com.carecloud.carepaylibray.payments.models.LocationDTO;
@@ -93,6 +94,7 @@ public class PaymentsActivity extends BasePracticeActivity implements FilterDial
                     String.format(Locale.getDefault(), "%1s", patientBalancesList.size()));
         }
 
+        findViewById(R.id.practice_payment_find_patient).setOnClickListener(onFindPatientClick());
         findViewById(R.id.practice_payment_filter_label).setOnClickListener(onFilterIconClick());
         findViewById(R.id.practice_payment_go_back).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -186,6 +188,17 @@ public class PaymentsActivity extends BasePracticeActivity implements FilterDial
                         findViewById(R.id.activity_practice_payment), filter);
 
                 filterDialog.showPopWindow();
+            }
+        };
+    }
+
+    private View.OnClickListener onFindPatientClick() {
+        return new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                FindPatientDialog findPatientDialog = new FindPatientDialog(PaymentsActivity.this, paymentsModel);
+                findPatientDialog.show();
             }
         };
     }
---
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/payments/dialogs/FindPatientDialog.java
@@ -0,0 +1,123 @@
+package com.carecloud.carepay.practice.library.payments.dialogs;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.carecloud.carepay.practice.library.R;
+import com.carecloud.carepay.service.library.WorkflowServiceCallback;
+import com.carecloud.carepay.service.library.WorkflowServiceHelper;
+import com.carecloud.carepay.service.library.constants.ApplicationMode;
+import com.carecloud.carepay.service.library.dtos.TransitionDTO;
+import com.carecloud.carepay.service.library.dtos.WorkflowDTO;
+import com.carecloud.carepaylibray.payments.models.PaymentsLabelDTO;
+import com.carecloud.carepaylibray.payments.models.PaymentsModel;
+import com.google.gson.JsonArray;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindPatientDialog extends Dialog {
+
+    private Context context;
+    private PaymentsModel paymentsModel;
+
+    public FindPatientDialog(Context context, PaymentsModel paymentsModel) {
+        super(context);
+        this.context = context;
+        this.paymentsModel = paymentsModel;
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_find_patient);
+        setCancelable(false);
+        getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.height = LinearLayout.LayoutParams.MATCH_PARENT;
+        params.width = LinearLayout.LayoutParams.MATCH_PARENT;
+        getWindow().setAttributes(params);
+
+        initializeView();
+    }
+
+    private void initializeView() {
+        PaymentsLabelDTO paymentsLabel = paymentsModel.getPaymentsMetadata().getPaymentsLabel();
+
+        EditText findPatientEditBox = (EditText) findViewById(R.id.find_patient_edit_box);
+        findPatientEditBox.setHint(paymentsLabel.getPracticePaymentsFindPatientLabel());
+        setTextListener(findPatientEditBox);
+
+        ((TextView) findViewById(R.id.find_patient_close_label))
+                .setText(paymentsLabel.getPracticePaymentsDetailDialogCloseButton());
+
+        findViewById(R.id.find_patient_close_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+    }
+
+    private void setTextListener(final EditText editView) {
+        editView.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Map<String, String> queryMap = new HashMap<>();
+                queryMap.put("practice_mgmt", ApplicationMode.getInstance().getUserPracticeDTO().getPracticeMgmt());
+                queryMap.put("practice_id", ApplicationMode.getInstance().getUserPracticeDTO().getPracticeId());
+
+                JsonArray postModel = new JsonArray();
+                postModel.add(editView.getText().toString());
+                String postBody = postModel.toString();
+
+                TransitionDTO transitionDTO = paymentsModel.getPaymentsMetadata().getPaymentsLinks().getFindPatient();
+                WorkflowServiceHelper.getInstance().execute(transitionDTO, findPatientCallback, postBody, queryMap);
+            }
+        });
+    }
+
+    private WorkflowServiceCallback findPatientCallback = new WorkflowServiceCallback() {
+
+        @Override
+        public void onPreExecute() {
+
+        }
+
+        @Override
+        public void onPostExecute(WorkflowDTO workflowDTO) {
+            System.out.println(workflowDTO.toString());
+        }
+
+        @Override
+        public void onFailure(String exceptionMessage) {
+
+        }
+    };
+}
