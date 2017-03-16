dc9566a04827b81f784709cfea55a44b19111045
cocampo-carecloud <cocampo@carecloud.com>
2017-03-16 15:31:27 -0400

    Removed unnecessary import
    Removed unnecessary import

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/appointments/dialogs/PracticeAppointmentDialog.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/appointments/dialogs/PracticeAppointmentDialog.java
@@ -10,7 +10,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
