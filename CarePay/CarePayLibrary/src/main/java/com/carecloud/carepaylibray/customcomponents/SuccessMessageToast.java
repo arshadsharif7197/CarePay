58710384a6aa89eec44371ff93cc1313ec1aaef6
Kavin Kannan <kkannan@carecloud.com>
2017-03-20 21:31:55 -0400

    SHMRK-2215 Removing unused import

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/customcomponents/SuccessMessageToast.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/customcomponents/SuccessMessageToast.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.carecloud.carepay.service.library.constants.ApplicationMode;
 import com.carecloud.carepaylibrary.R;
 import com.carecloud.carepaylibray.utils.StringUtil;
 
