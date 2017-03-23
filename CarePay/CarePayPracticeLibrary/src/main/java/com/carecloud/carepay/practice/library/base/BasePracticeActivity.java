e049bb926d45d70417a22e65122c562a4ba86089
kkannan-carecloud <kkannan@carecloud.com>
2017-03-23 17:33:10 -0400

    Fixing indentation.

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/base/BasePracticeActivity.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/base/BasePracticeActivity.java
@@ -84,7 +84,7 @@ public abstract class BasePracticeActivity extends BaseActivity
      */
     public void setSystemUiVisibility() {
             getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                              View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
