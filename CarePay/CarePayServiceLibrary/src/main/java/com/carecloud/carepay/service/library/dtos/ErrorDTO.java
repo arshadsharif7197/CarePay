4f4af928b7e9e5577209b7d986f75c1a14ee7a79
kkannan-carecloud <kkannan@carecloud.com>
2017-03-30 16:32:32 -0400

    SHMRK-2578 DTO Classses for validation Error https://jira.carecloud.com/browse/SHMRK-2578

--- CarePay/CarePayServiceLibrary/src/main/java/com/carecloud/carepay/service/library/dtos/ErrorDTO.java
+++ CarePay/CarePayServiceLibrary/src/main/java/com/carecloud/carepay/service/library/dtos/ErrorDTO.java
@@ -4,7 +4,7 @@ package com.carecloud.carepay.service.library.dtos;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class ServiceErrorDTO {
+public class ErrorDTO {
 
     @SerializedName("error_code")
     @Expose
