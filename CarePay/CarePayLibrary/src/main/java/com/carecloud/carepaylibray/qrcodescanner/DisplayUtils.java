833971a57ae4aeed5f4a828b813a0cec97e0f6bf
cocampo-carecloud <cocampo@carecloud.com>
2017-03-30 12:49:34 -0400

    Camera Orientation fix
    Camera Orientation fix

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/qrcodescanner/DisplayUtils.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/qrcodescanner/DisplayUtils.java
@@ -3,21 +3,41 @@ package com.carecloud.carepaylibray.qrcodescanner;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Point;
+import android.hardware.Camera;
 import android.view.Display;
+import android.view.Surface;
 import android.view.WindowManager;
 
 public class DisplayUtils {
-    public static Point getScreenResolution(Context context) {
-        WindowManager systemService = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display = systemService.getDefaultDisplay();
-        Point screenResolution = new Point();
-        if (android.os.Build.VERSION.SDK_INT >= 13) {
-            display.getSize(screenResolution);
+    public static int getDisplayOrientation(Context context, int cameraId) {
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        if(cameraId == -1) {
+            Camera.getCameraInfo(Camera.CameraInfo.CAMERA_FACING_BACK, info);
         } else {
-            screenResolution.set(display.getWidth(), display.getHeight());
+            Camera.getCameraInfo(cameraId, info);
         }
 
-        return screenResolution;
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+
+        int rotation = display.getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+
+        return result;
     }
 
     public static int getScreenOrientation(Context context)
