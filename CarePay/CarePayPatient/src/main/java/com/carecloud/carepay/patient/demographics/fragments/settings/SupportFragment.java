0d8f6fc0bcbeed7bd874e68e1a217827b89ed858
cocampo-carecloud <cocampo@carecloud.com>
2017-04-04 17:15:54 -0400

    Part 6
    Part 6

--- CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/demographics/fragments/settings/SupportFragment.java
+++ CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/demographics/fragments/settings/SupportFragment.java
@@ -2,10 +2,19 @@ package com.carecloud.carepay.patient.demographics.fragments.settings;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.URLSpan;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,12 +33,12 @@ public class SupportFragment extends BaseFragment {
     public static final String PHONE = "tel:18552044075";
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle icicle){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle icicle) {
         return inflater.inflate(R.layout.fragment_support, container, false);
     }
 
     @Override
-    public void onViewCreated(View view, Bundle icicle){
+    public void onViewCreated(View view, Bundle icicle) {
         Toolbar toolbar = (Toolbar) view.findViewById(R.id.settings_toolbar);
         toolbar.setNavigationIcon(ContextCompat.getDrawable(getActivity(), R.drawable.icn_nav_back));
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
@@ -37,8 +46,30 @@ public class SupportFragment extends BaseFragment {
         TextView title = (TextView) toolbar.findViewById(R.id.settings_toolbar_title);
         title.setText(Label.getLabel("support_label"));
 
+        initializeHelpContent(view);
         initializeEmailButton(view);
         initializePhoneButton(view);
+    }
+
+    private void initializeHelpContent(View view) {
+        TextView textView = (TextView) view.findViewById(R.id.support_help_content);
+        String html = Label.getLabel("support_help_content");
+
+        CharSequence sequence;
+        if (Build.VERSION.SDK_INT > 23) {
+            sequence = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            sequence = Html.fromHtml(html);
+        }
+
+
+        SpannableStringBuilder strBuilder = new SpannableStringBuilder(sequence);
+        URLSpan[] urls = strBuilder.getSpans(0, sequence.length(), URLSpan.class);
+        for (URLSpan span : urls) {
+            makeLinkClickable(strBuilder, span);
+        }
+        textView.setText(strBuilder);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     private void initializeEmailButton(View view) {
@@ -46,9 +77,7 @@ public class SupportFragment extends BaseFragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", EMAIL, null));
-                intent.putExtra(Intent.EXTRA_SUBJECT, SUBJECT);
-                startActivity(Intent.createChooser(intent, CHOOSE_LABEL));
+                openEmailIntent();
             }
         });
     }
@@ -62,5 +91,30 @@ public class SupportFragment extends BaseFragment {
                 startActivity(Intent.createChooser(intent, CHOOSE_LABEL));
             }
         });
+    }
+
+    private void makeLinkClickable(SpannableStringBuilder strBuilder, final URLSpan span) {
+        int start = strBuilder.getSpanStart(span);
+        int end = strBuilder.getSpanEnd(span);
+        int flags = strBuilder.getSpanFlags(span);
+        ClickableSpan clickable = new ClickableSpan() {
+            public void onClick(View view) {
+                openEmailIntent();
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(false);
+            }
+        };
+        strBuilder.setSpan(clickable, start, end, flags);
+        strBuilder.removeSpan(span);
+    }
+
+    private void openEmailIntent() {
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", EMAIL, null));
+        intent.putExtra(Intent.EXTRA_SUBJECT, SUBJECT);
+        startActivity(Intent.createChooser(intent, CHOOSE_LABEL));
     }
 }
