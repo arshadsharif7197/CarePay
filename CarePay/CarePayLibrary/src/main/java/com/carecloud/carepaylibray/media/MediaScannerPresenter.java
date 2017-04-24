f199c2fdffa12bb94feba0053f17a5a88c8db36e
lmenendez-carecloud <lmenendez@carecloud.com>
2017-04-24 16:25:20 -0400

    SHMRK-2198 Setup MediaScaner Presenter

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/media/MediaScannerPresenter.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/media/MediaScannerPresenter.java
@@ -1,19 +1,27 @@
 package com.carecloud.carepaylibray.media;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.carecloud.carepay.service.library.label.Label;
+import com.carecloud.carepaylibray.carepaycamera.CarePayCameraCallback;
 import com.carecloud.carepaylibray.carepaycamera.CarePayCameraReady;
 import com.carecloud.carepaylibray.utils.ImageCaptureHelper;
 import com.carecloud.carepaylibray.utils.PermissionsUtil;
+import com.carecloud.carepaylibray.utils.StringUtil;
+
+import static com.carecloud.carepaylibray.keyboard.KeyboardHolderActivity.LOG_TAG;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +31,7 @@ import java.util.List;
  *
  */
 
-public class MediaScannerAdapter {
+public class MediaScannerPresenter {
 
     public static final String ACTION_PICTURE = "demographics_take_pic_option";
     public static final String ACTION_GALLERY = "demographics_select_gallery_option";
@@ -33,32 +41,65 @@ public class MediaScannerAdapter {
     private MediaViewInterface mediaViewInterface;
     private CarePayCameraReady cameraReady;
 
-    public MediaScannerAdapter(Context context, CarePayCameraReady cameraReady, MediaViewInterface mediaViewInterface){
+    private String pendingAction;
+
+    public MediaScannerPresenter(Context context, CarePayCameraReady cameraReady, MediaViewInterface mediaViewInterface){
         this.context = context;
         this.cameraReady = cameraReady;
         this.mediaViewInterface = mediaViewInterface;
     }
 
 
-    /**
-     * Starts Camera or Gallery to capture/select an image
-     *
-     * @param isFrontScan The camera helper used with a particular imageview
-     */
-    public void selectImage(ImageView targetImageView, boolean isFrontScan, boolean includeGalleryOption) {
-//        this.targetImageView = targetImageView;
-//        this.isFrontScan = isFrontScan;
-
+    public void selectImage(boolean includeGalleryOption) {
         // create the chooser dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle(ImageCaptureHelper.chooseActionDlgTitle);
+        String title = StringUtil.captialize(Label.getLabel("demographics_select_capture_option_title"));
+        builder.setTitle(title);
 
         List<String> mediaOptions = getMediaOptions(includeGalleryOption);
         MediaActionAdapter mediaActionAdapter = new MediaActionAdapter(context, mediaOptions);
         builder.setAdapter(mediaActionAdapter, getActionItemClickListener(mediaOptions));
+
+        builder.setCancelable(false);
         builder.show();
     }
 
+    public void handleRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode) {
+            case PermissionsUtil.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && pendingAction.equals(ACTION_GALLERY)) {
+                    handleGalleryAction();
+                } else {
+                    //code for deny
+                    Log.v(LOG_TAG, "read external denied");
+                }
+                break;
+
+            case PermissionsUtil.MY_PERMISSIONS_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && pendingAction.equals(ACTION_PICTURE)) {
+                    handlePictureAction();
+                } else {
+                    //code for deny
+                    Log.v(LOG_TAG, "camera denied");
+                }
+        }
+
+    }
+
+    public void handleActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode == Activity.RESULT_OK) {
+            Bitmap bitmap = null;
+            if (requestCode == ImageCaptureHelper.SELECT_FILE) {
+               //TODO bitmap = ImageCaptureHelper.onSelectFromGalleryResult(context, targetImageView, data, cameraType, getImageShape());
+            }
+
+            if(bitmap != null) {
+                mediaViewInterface.getCameraCallback().onCapturedSuccess(bitmap);
+            }else{
+                mediaViewInterface.getCameraCallback().onCaptureFail();
+            }
+        }
+    }
 
     private DialogInterface.OnClickListener getActionItemClickListener(final List<String> mediaOptions) {
         return new DialogInterface.OnClickListener() {
@@ -67,22 +108,39 @@ public class MediaScannerAdapter {
                 String option = mediaOptions.get(which);
                 switch(option){
                     case ACTION_PICTURE:
-
+                        handlePictureAction();
                         break ;
                     case ACTION_GALLERY:
-
+                        handleGalleryAction();
                         break;
                     default:
-
+                        dialog.cancel();
 
                 }
             }
         };
     }
 
+    private void setPendingAction(String action){
+        this.pendingAction = action;
+    }
+
     private void handlePictureAction(){
         if(PermissionsUtil.checkPermissionCamera(context)){
+            CarePayCameraCallback cameraCallback = mediaViewInterface.getCameraCallback();
+            if(cameraCallback!=null) {
+                cameraReady.captureImage(cameraCallback);
+            }
+        }else{
+            setPendingAction(ACTION_PICTURE);
+        }
+    }
 
+    private void handleGalleryAction(){
+        if(PermissionsUtil.checkPermission(context)){
+            mediaViewInterface.startActivityForResult(ImageCaptureHelper.galleryIntent(), ImageCaptureHelper.SELECT_FILE);
+        }else{
+            setPendingAction(ACTION_GALLERY);
         }
     }
 
--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/media/MediaViewInterface.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/media/MediaViewInterface.java
@@ -1,5 +1,9 @@
 package com.carecloud.carepaylibray.media;
 
+import android.content.Intent;
+
+import com.carecloud.carepaylibray.carepaycamera.CarePayCameraCallback;
+
 /**
  * Created by lmenendez on 4/24/17.
  */
@@ -8,4 +12,9 @@ public interface MediaViewInterface {
 
     void handleRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults);
 
+    CarePayCameraCallback getCameraCallback();
+
+    void startActivityForResult(Intent intent, int requestCode);
+
+    void onActivityResult(int requestCode, int resultCode, Intent data);
 }
