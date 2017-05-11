bbc519a82b96799df63e709f53862d131695c267
Pablo Johnson <pablo.88j@gmail.com>
2017-05-11 11:44:54 -0500

    SHMRK-2325

--- CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/signinsignuppatient/SigninSignupActivity.java
+++ CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/signinsignuppatient/SigninSignupActivity.java
@@ -1,5 +1,6 @@
 package com.carecloud.carepay.patient.signinsignuppatient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,6 +9,7 @@ import android.view.MenuItem;
 import com.carecloud.carepay.patient.R;
 import com.carecloud.carepay.patient.base.BasePatientActivity;
 import com.carecloud.carepay.patient.signinsignuppatient.fragments.SigninFragment;
+import com.carecloud.carepay.patient.tutorial.tutorial.TutorialActivity;
 import com.carecloud.carepaylibray.interfaces.DTO;
 import com.carecloud.carepaylibray.interfaces.FragmentActivityInterface;
 import com.carecloud.carepaylibray.signinsignup.dto.SignInDTO;
@@ -18,7 +20,6 @@ import com.carecloud.carepaylibray.utils.SystemUtil;
  * Activity supporting Signin and Sign-up
  */
 public class SigninSignupActivity extends BasePatientActivity implements FragmentActivityInterface {
-
 
     private SignInDTO signInSignUpDTO;
 
@@ -31,6 +32,10 @@ public class SigninSignupActivity extends BasePatientActivity implements Fragmen
             fragmentManager.beginTransaction()
                     .replace(R.id.layoutSigninSignup, new SigninFragment(), SigninFragment.class.getSimpleName())
                     .commit();
+        }
+        if (!getApplicationPreferences().isTutorialShown()) {
+            startActivity(new Intent(getContext(), TutorialActivity.class));
+            getApplicationPreferences().setTutorialShown(true);
         }
     }
 
