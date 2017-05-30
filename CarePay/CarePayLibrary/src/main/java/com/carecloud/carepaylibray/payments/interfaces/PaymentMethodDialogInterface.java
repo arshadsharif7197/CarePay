c5843ca189710006dbe9824c8b965687a3bfaff1
lmenendez-carecloud <lmenendez@carecloud.com>
2017-05-30 16:24:17 -0400

    SHMRK-3142 - ccc

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/payments/interfaces/PaymentMethodDialogInterface.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/payments/interfaces/PaymentMethodDialogInterface.java
@@ -3,7 +3,7 @@ package com.carecloud.carepaylibray.payments.interfaces;
 import com.carecloud.carepaylibray.payments.models.PaymentsModel;
 
 /**
- * Created by lmenendez on 5/30/17.
+ * Created by lmenendez on 5/30/17
  */
 
 public interface PaymentMethodDialogInterface extends PaymentMethodInterface {
