edf759784dfa93cab1dd960d3a522cb15186d5db
Pablo Johnson <pablo.88j@gmail.com>
2017-06-08 17:07:45 -0500

    go to practice mode

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/patientmodecheckin/activities/CompleteCheckActivity.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/patientmodecheckin/activities/CompleteCheckActivity.java
@@ -3,16 +3,24 @@ package com.carecloud.carepay.practice.library.patientmodecheckin.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
 import com.carecloud.carepay.practice.library.R;
 import com.carecloud.carepay.practice.library.base.BasePracticeActivity;
+import com.carecloud.carepay.practice.library.base.PracticeNavigationHelper;
 import com.carecloud.carepay.practice.library.customdialog.ConfirmationPinDialog;
 import com.carecloud.carepay.practice.library.patientmodecheckin.fragments.CheckInCompletedDialogFragment;
 import com.carecloud.carepay.practice.library.patientmodecheckin.interfaces.CheckCompleteInterface;
+import com.carecloud.carepay.service.library.CarePayConstants;
+import com.carecloud.carepay.service.library.WorkflowServiceCallback;
+import com.carecloud.carepay.service.library.dtos.TransitionDTO;
 import com.carecloud.carepay.service.library.dtos.WorkflowDTO;
 import com.carecloud.carepaylibray.appointments.models.AppointmentsResultModel;
 import com.carecloud.carepaylibray.interfaces.DTO;
 import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author pjohnson on 8/06/17.
@@ -61,4 +69,33 @@ public class CompleteCheckActivity extends BasePracticeActivity implements Check
                 ((AppointmentsResultModel) dto).getMetadata().getLinks().getPinpad(), false);
         confirmationPinDialog.show();
     }
+
+    @Override
+    public void onPinConfirmationCheck(boolean isCorrectPin, String pin) {
+        TransitionDTO transitionDTO = ((AppointmentsResultModel) dto).getMetadata().getTransitions().getPracticeMode();
+        Map<String, String> query = new HashMap<>();
+        query.put("practice_mgmt", getApplicationMode().getUserPracticeDTO().getPracticeMgmt());
+        query.put("practice_id", getApplicationMode().getUserPracticeDTO().getPracticeId());
+        getWorkflowServiceHelper().execute(transitionDTO, patientHomeCallback, query);
+    }
+
+    WorkflowServiceCallback patientHomeCallback = new WorkflowServiceCallback() {
+        @Override
+        public void onPreExecute() {
+            showProgressDialog();
+        }
+
+        @Override
+        public void onPostExecute(WorkflowDTO workflowDTO) {
+            hideProgressDialog();
+            PracticeNavigationHelper.navigateToWorkflow(getContext(), workflowDTO);
+        }
+
+        @Override
+        public void onFailure(String exceptionMessage) {
+            hideProgressDialog();
+            showErrorNotification(CarePayConstants.CONNECTION_ISSUE_ERROR_MESSAGE);
+            Log.e(getString(com.carecloud.carepaylibrary.R.string.alert_title_server_error), exceptionMessage);
+        }
+    };
 }
