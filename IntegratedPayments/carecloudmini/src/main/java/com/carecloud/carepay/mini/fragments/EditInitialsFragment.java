5003228c06b7185de0590c68d2d0230d93a794fe
lmenendez-carecloud <lmenendez@carecloud.com>
2017-07-12 13:38:48 -0400

    SHMRK-3054 - update practices fragment to send practice mgmt when selecting practice - prevent clicking practice list while making post call - add edit initials fragment - update select image fragment to match latest mockup - add review fragment

---
+++ IntegratedPayments/carecloudmini/src/main/java/com/carecloud/carepay/mini/fragments/EditInitialsFragment.java
@@ -0,0 +1,91 @@
+package com.carecloud.carepay.mini.fragments;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.carecloud.carepay.mini.R;
+import com.carecloud.carepay.mini.models.response.UserPracticeDTO;
+
+/**
+ * Created by lmenendez on 7/11/17
+ */
+
+public class EditInitialsFragment extends RegistrationFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle icicle) {
+        return inflater.inflate(R.layout.fragment_registration_initials, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle icicle) {
+        initToolbar(view);
+
+        final UserPracticeDTO selectedPractice = callback.getRegistrationDataModel().getPayloadDTO().getUserPractices().get(0);
+
+        EditText practiceInitials = (EditText) view.findViewById(R.id.practice_initials_name);
+        practiceInitials.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    selectedPractice.setPracticeInitials(textView.getText().toString());
+                    callback.replaceFragment(new ImageSelectFragment(), false);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        practiceInitials.setText(selectedPractice.getPracticeInitials());
+        practiceInitials.addTextChangedListener(capsInputWatcher);
+        practiceInitials.requestFocus();
+    }
+
+    private void initToolbar(View view){
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.registration_toolbar);
+        toolbar.setNavigationIcon(R.drawable.close_blue);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callback.replaceFragment(new ImageSelectFragment(), false);
+            }
+        });
+
+        TextView title = (TextView) view.findViewById(R.id.toolbar_title);
+        title.setText(getString(R.string.registration_image_edit_initials));
+    }
+
+    private TextWatcher capsInputWatcher = new TextWatcher() {
+        boolean shiftCase = false;
+
+        @Override
+        public void beforeTextChanged(CharSequence sequence, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence sequence, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if(!shiftCase && editable.length() > 0){
+                shiftCase = true;
+                editable.replace(0, editable.length(), editable.toString().toUpperCase());
+            }else{
+                shiftCase = false;
+            }
+        }
+    };
+
+}
