635162d29e8eddb7cc982c45b35f7291df899f29
Pablo Johnson <pablo.88j@gmail.com>
2017-09-07 15:59:29 -0500

    SHMRK-3652 (PracticeApp)
    - Add home button back to check in flow

---
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/checkin/dialog/HomeAlertDialogFragment.java
@@ -0,0 +1,63 @@
+package com.carecloud.carepay.practice.library.checkin.dialog;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.carecloud.carepay.practice.library.R;
+import com.carecloud.carepaylibray.base.BaseDialogFragment;
+
+/**
+ * @author pjohnson
+ */
+public class HomeAlertDialogFragment extends BaseDialogFragment {
+
+
+    private HomeAlertInterface callback;
+
+    public HomeAlertDialogFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeAlertDialogFragment newInstance() {
+        return new HomeAlertDialogFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home_alert_dialog, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button yesButton = (Button) view.findViewById(R.id.button_right_action);
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callback.onAcceptExit();
+            }
+        });
+        Button noButton = (Button) view.findViewById(R.id.button_left_action);
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+
+    public void setCallback(HomeAlertInterface homeAlertInterface) {
+        this.callback = homeAlertInterface;
+    }
+
+    public interface HomeAlertInterface {
+        void onAcceptExit();
+    }
+}
