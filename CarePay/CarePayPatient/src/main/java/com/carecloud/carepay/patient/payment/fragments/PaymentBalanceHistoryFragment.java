b6b27254f4c833cdf04a69a3a6e1950fba20ddde
lmenendez-carecloud <lmenendez@carecloud.com>
2017-10-03 09:47:06 -0400

    SHMRK-4185 (PatientApp) - kick code climate

--- CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/payment/fragments/PaymentBalanceHistoryFragment.java
+++ CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/payment/fragments/PaymentBalanceHistoryFragment.java
@@ -82,6 +82,7 @@ public class PaymentBalanceHistoryFragment extends BaseFragment {
     private void setTabTitle(TabLayout.Tab tab, String title){
         TextView textView = (TextView) tab.getCustomView().findViewById(R.id.tab_title);
         textView.setText(title);
+
     }
 
     @Override
