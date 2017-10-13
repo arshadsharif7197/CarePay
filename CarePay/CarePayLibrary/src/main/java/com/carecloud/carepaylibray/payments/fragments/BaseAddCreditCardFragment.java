9cd8f50916b49ac9ea884b5efa733e1d981145c4
lmenendez-carecloud <lmenendez@carecloud.com>
2017-10-13 17:31:49 -0400

    SHMRK-4286 (PracticeApp) - allow adding card even if profile address is empty

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/payments/fragments/BaseAddCreditCardFragment.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/payments/fragments/BaseAddCreditCardFragment.java
@@ -4,7 +4,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -528,7 +527,7 @@ public abstract class BaseAddCreditCardFragment extends BasePaymentDialogFragmen
             stateAutoCompleteTextView.getOnFocusChangeListener().onFocusChange(stateAutoCompleteTextView,
                     !StringUtil.isNullOrEmpty(addressPayloadDTO.getState()));
         } else {
-            useProfileAddressCheckBox.setChecked(false);
+//            useProfileAddressCheckBox.setChecked(false);
             useProfileAddressCheckBox.setEnabled(false);
             useProfileAddressCheckBox.setVisibility(View.GONE);
             setAddressFieldsEnabled(true);
