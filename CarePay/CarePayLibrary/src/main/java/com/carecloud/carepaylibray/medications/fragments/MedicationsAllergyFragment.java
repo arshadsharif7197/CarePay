97833fa8e6e05cf9c33818a2d884d69a234877d5
lmenendez-carecloud <lmenendez@carecloud.com>
2017-11-16 15:35:05 -0500

    SHMRK-3109 (PatientApp)(PracticeApp) - add Med Image payload - init image from payload

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/medications/fragments/MedicationsAllergyFragment.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/medications/fragments/MedicationsAllergyFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.NestedScrollView;
@@ -455,8 +456,15 @@ public class MedicationsAllergyFragment extends BaseCheckinFragment implements
     }
 
     @Override
-    public void handleRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        handleRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
+    @Override
+    public void handleRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (mediaScannerPresenter != null) {
+            mediaScannerPresenter.handleRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     @Override
@@ -466,13 +474,13 @@ public class MedicationsAllergyFragment extends BaseCheckinFragment implements
 
     @Override
     public void handleStartActivityForResult(Intent intent, int requestCode) {
-
+        startActivityForResult(intent, requestCode);
     }
 
     @Nullable
     @Override
     public Fragment getCallingFragment() {
-        return null;
+        return this;
     }
 
     @Override
