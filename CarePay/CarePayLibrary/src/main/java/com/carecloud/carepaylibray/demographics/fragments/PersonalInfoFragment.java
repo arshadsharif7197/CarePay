49654328deef85afd917632af15ca9a1ba004770
lmenendez-carecloud <lmenendez@carecloud.com>
2018-03-08 17:51:50 -0500

    FEATURE-PaymentPlans (PatientApp)(PracticeApp) - add email validation null check (not Cherry Picked) - hide phone type in demo checkin

--- CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/demographics/fragments/PersonalInfoFragment.java
+++ CarePay/CarePayLibrary/src/main/java/com/carecloud/carepaylibray/demographics/fragments/PersonalInfoFragment.java
@@ -155,6 +155,7 @@ public class PersonalInfoFragment extends CheckInDemographicsBaseFragment implem
         phoneNumberEditText.setOnClickListener(selectEndOnClick);
 
 
+/*
         TextInputLayout phoneTypeInputLayout = (TextInputLayout) view.findViewById(R.id.phoneTypeInputLayout);
         phoneNumberTypeEditText = (EditText) view.findViewById(R.id.phoneTypeEditText);
         phoneNumberTypeEditText.setOnFocusChangeListener(SystemUtil.getHintFocusChangeListener(phoneTypeInputLayout, null));
@@ -167,6 +168,7 @@ public class PersonalInfoFragment extends CheckInDemographicsBaseFragment implem
         initSelectableInput(phoneNumberTypeEditText, selectedPhoneType,
                 demographicPayload.getAddress().getPhoneNumberType(), null);
         phoneNumberTypeEditText.setText(demographicPayload.getAddress().getPhoneNumberType());
+*/
 
     }
 
@@ -262,6 +264,7 @@ public class PersonalInfoFragment extends CheckInDemographicsBaseFragment implem
                 unsetFieldError(phoneLayout);
             }
 
+/*
             if (validateField(view, dataModel.getDemographic().getAddress().getProperties()
                             .getPhoneType().isRequired(), selectedPhoneType.getName(), R.id.phoneNumberContainer,
                     R.id.phoneTypeInputLayout, isUserAction())) {
@@ -269,6 +272,7 @@ public class PersonalInfoFragment extends CheckInDemographicsBaseFragment implem
             } else {
                 unsetFieldError((TextInputLayout) view.findViewById(R.id.phoneTypeInputLayout));
             }
+*/
 
             return true;
         } finally {
