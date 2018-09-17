9795074ffe9410b771a6e0b5655506050cd90fb2
Pablo Johnson <pjohnson@carecloud.com>
2018-09-17 12:33:59 -0500

    SHMRK-6581
    Deleting old and unused classes

--- CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/survey/model/SurveyModelMetadata.java
+++ CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/survey/model/SurveyModelMetadata.java
@@ -1,62 +1,65 @@
-package com.carecloud.carepay.practice.library.checkin.dtos;
+package com.carecloud.carepay.patient.survey.model;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 /**
- * Created by sudhir_pingale on 11/11/2016.
+ * @author pjohnson on 17/09/18.
  */
+public class SurveyModelMetadata {
 
-public class CheckInStatusDataPayloadMetadataDTO {
-
+    @Expose
     @SerializedName("practice_mgmt")
-    @Expose
     private String practiceMgmt;
+    @Expose
     @SerializedName("practice_id")
-    @Expose
     private String practiceId;
-    @SerializedName("appointment_id")
     @Expose
+    @SerializedName("created_dt")
+    private String createdDate;
+    @Expose
+    @SerializedName("visit_dt")
+    private String visitDate;
+    @Expose
+    @SerializedName("appointment_id")
     private String appointmentId;
 
-    /**
-     * @return The practiceMgmt
-     */
     public String getPracticeMgmt() {
         return practiceMgmt;
     }
 
-    /**
-     * @param practiceMgmt The practice_mgmt
-     */
     public void setPracticeMgmt(String practiceMgmt) {
         this.practiceMgmt = practiceMgmt;
     }
 
-    /**
-     * @return The practiceId
-     */
     public String getPracticeId() {
         return practiceId;
     }
 
-    /**
-     * @param practiceId The practice_id
-     */
     public void setPracticeId(String practiceId) {
         this.practiceId = practiceId;
     }
 
-    /**
-     * @return The appointmentId
-     */
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getVisitDate() {
+        return visitDate;
+    }
+
+    public void setVisitDate(String visitDate) {
+        this.visitDate = visitDate;
+    }
+
     public String getAppointmentId() {
         return appointmentId;
     }
 
-    /**
-     * @param appointmentId The appointment_id
-     */
     public void setAppointmentId(String appointmentId) {
         this.appointmentId = appointmentId;
     }
