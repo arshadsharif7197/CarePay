bb99bdc36d31434a2967cd149bcc1faa509899ed
Pablo Johnson <pjohnson@carecloud.com>
2018-12-13 18:19:18 -0500

    SHMRK-7610
    Make sure pending/checkin in progress appointments do not show on historical appointments (front end filtering)

--- CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/appointments/fragments/AppointmentHistoryFragment.java
+++ CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/appointments/fragments/AppointmentHistoryFragment.java
@@ -17,6 +17,7 @@ import com.carecloud.carepay.patient.appointments.adapters.PracticesAdapter;
 import com.carecloud.carepay.patient.appointments.presenter.PatientAppointmentPresenter;
 import com.carecloud.carepay.patient.base.ShimmerFragment;
 import com.carecloud.carepay.service.library.ApplicationPreferences;
+import com.carecloud.carepay.service.library.CarePayConstants;
 import com.carecloud.carepay.service.library.WorkflowServiceCallback;
 import com.carecloud.carepay.service.library.dtos.UserPracticeDTO;
 import com.carecloud.carepay.service.library.dtos.WorkflowDTO;
@@ -54,6 +55,7 @@ public class AppointmentHistoryFragment extends BaseFragment
     private boolean isPaging;
     private Paging paging;
     private UserPracticeDTO selectedPractice;
+    private List<String> excludedAppointmentStates;
 
     public static AppointmentHistoryFragment newInstance() {
         return new AppointmentHistoryFragment();
@@ -87,6 +89,10 @@ public class AppointmentHistoryFragment extends BaseFragment
         });
         selectedPractice = appointmentDto.getPayload().getUserPractices().get(0);
         callAppointmentService(selectedPractice, true, true);
+        excludedAppointmentStates = new ArrayList<>();
+        excludedAppointmentStates.add(CarePayConstants.PENDING);
+        excludedAppointmentStates.add(CarePayConstants.REQUESTED);
+        excludedAppointmentStates.add(CarePayConstants.CHECKING_IN);
     }
 
     @Nullable
@@ -259,7 +265,18 @@ public class AppointmentHistoryFragment extends BaseFragment
     private void showHistoricAppointments(List<AppointmentDTO> appointments, boolean refresh) {
         getView().findViewById(R.id.noAppointmentsLayout).setVisibility(View.GONE);
         historicAppointmentsRecyclerView.setVisibility(View.VISIBLE);
+        appointments = filterAppointments(appointments);
         adapter.setData(appointments, refresh);
+    }
+
+    private List<AppointmentDTO> filterAppointments(List<AppointmentDTO> appointments) {
+        List<AppointmentDTO> filteredAppointments = new ArrayList<>();
+        for (AppointmentDTO appointment : appointments) {
+            if (!excludedAppointmentStates.contains(appointment.getPayload().getAppointmentStatus().getCode())) {
+                filteredAppointments.add(appointment);
+            }
+        }
+        return filteredAppointments;
     }
 
     @Override
