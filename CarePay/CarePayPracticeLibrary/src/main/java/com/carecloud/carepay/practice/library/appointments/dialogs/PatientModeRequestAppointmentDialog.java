d5125d366475ba94663d3d9200b27584f568716d
lmenendez-carecloud <lmenendez@carecloud.com>
2019-04-05 18:19:31 -0400

    SHMRK-8724 - show video visit icon in request appt for patient mode - show cancel icon instead of back for patient mode req appt flow

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/appointments/dialogs/PatientModeRequestAppointmentDialog.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/appointments/dialogs/PatientModeRequestAppointmentDialog.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ScrollView;
@@ -22,7 +21,6 @@ import com.carecloud.carepaylibray.appointments.models.VisitTypeDTO;
 import com.carecloud.carepaylibray.utils.DateUtil;
 import com.carecloud.carepaylibray.utils.DtoHelper;
 import com.carecloud.carepaylibray.utils.MixPanelUtil;
-import com.carecloud.carepaylibray.utils.PicassoHelper;
 import com.carecloud.carepaylibray.utils.PicassoHelper;
 import com.carecloud.carepaylibray.utils.StringUtil;
 import com.carecloud.carepaylibray.utils.SystemUtil;
@@ -97,6 +95,10 @@ public class PatientModeRequestAppointmentDialog extends BaseRequestAppointmentD
         VisitTypeDTO visitTypeDTO = appointmentDTO.getPayload().getVisitType();
         TextView visitTypeTextView = view.findViewById(R.id.reasonTextView);
         visitTypeTextView.setText(visitTypeDTO.getName());
+
+        View videoVisitIndicator = view.findViewById(R.id.visit_type_video);
+        videoVisitIndicator.setVisibility(appointmentDTO.getPayload().getVisitType()
+                .hasVideoOption() ? View.VISIBLE : View.GONE);
 
         View prepaidLayout = findViewById(R.id.prepaymentLayout);
         if (visitTypeDTO.getAmount() > 0) {
