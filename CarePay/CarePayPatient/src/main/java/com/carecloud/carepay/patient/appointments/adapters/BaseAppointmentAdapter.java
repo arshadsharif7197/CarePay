04f6b7269f46f39973ab7a7cf40aac7ab24e95bd
Doug Rodriguez <dorodriguez@carecloud.com>
2019-04-09 16:16:47 -0400

    SHMRK-8702
    - Adding pending label to requested appointments, removing from scheduled

--- CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/appointments/adapters/BaseAppointmentAdapter.java
+++ CarePay/CarePayPatient/src/main/java/com/carecloud/carepay/patient/appointments/adapters/BaseAppointmentAdapter.java
@@ -112,8 +112,6 @@ public abstract class BaseAppointmentAdapter extends RecyclerView.Adapter<BaseAp
                 break;
             }
             case PENDING: {
-                holder.todayTimeMessage.setVisibility(View.VISIBLE);
-                holder.todayTimeMessage.setText(Label.getLabel("appointment_status_pending"));
                 holder.todayTimeLayout.setVisibility(View.VISIBLE);
                 holder.todayTimeTextView.setText(dateUtil.getTime12Hour());
                 holder.todayTimeTextView.setTextColor(ContextCompat.getColor(context, R.color.emerald));
@@ -124,6 +122,8 @@ public abstract class BaseAppointmentAdapter extends RecyclerView.Adapter<BaseAp
                 break;
             }
             case REQUESTED: {
+                holder.todayTimeMessage.setVisibility(View.VISIBLE);
+                holder.todayTimeMessage.setText(Label.getLabel("appointment_status_pending"));
                 holder.todayTimeLayout.setVisibility(View.VISIBLE);
                 holder.todayTimeTextView.setText(dateUtil.getTime12Hour());
                 holder.todayTimeTextView.setTextColor(ContextCompat.getColor(context, R.color.lightning_yellow));
