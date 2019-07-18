fb4dc0d0765c8383b9f5c91b0416c88cc25d9941
Doug Rodriguez <dorodriguez@carecloud.com>
2019-07-18 11:39:21 -0400

    SHMRK-8771
    - Adding Migration

--- CarePay/CarePayPracticeClover/src/main/java/com/carecloud/carepay/practice/clover/BreezeDataBase.java
+++ CarePay/CarePayPracticeClover/src/main/java/com/carecloud/carepay/practice/clover/BreezeDataBase.java
@@ -3,6 +3,9 @@ package com.carecloud.carepay.practice.clover;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
 import android.content.Context;
 
 import com.carecloud.carepay.practice.clover.models.CloverPaymentQueueRecordDao;
@@ -12,7 +15,7 @@ import com.carecloud.carepay.practice.clover.models.CloverQueuePaymentRecord;
 /**
  * @author pjohnson on 2/27/19.
  */
-@Database(entities = {CloverQueuePaymentRecord.class}, version = 1, exportSchema = false)
+@Database(entities = {CloverQueuePaymentRecord.class}, version = 2, exportSchema = false)
 public abstract class BreezeDataBase extends RoomDatabase {
 
     private static volatile BreezeDataBase INSTANCE;
@@ -23,12 +26,21 @@ public abstract class BreezeDataBase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             BreezeDataBase.class, "clover_breeze_database")
+                            .addMigrations(MIGRATION_1_2)
                             .build();
                 }
             }
         }
         return INSTANCE;
     }
+
+    private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE CloverQueuePaymentRecord"
+                    + " ADD COLUMN patientUsername TEXT");
+        }
+    };
 
     public abstract CloverPaymentQueueRecordDao getCloverPaymentDao();
 }
