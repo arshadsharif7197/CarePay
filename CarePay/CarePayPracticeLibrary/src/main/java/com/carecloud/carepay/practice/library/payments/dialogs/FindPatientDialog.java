74bca73d3d6311e9d6109069caed438645b85609
arshadsharif7197 <ARSHADSHARIF@ISB.MTBC.COM>
2022-04-28 03:12:02 +0500

    BREEZ-1368 Android PR | Loader didn't show on patient search

--- CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/payments/dialogs/FindPatientDialog.java
+++ CarePay/CarePayPracticeLibrary/src/main/java/com/carecloud/carepay/practice/library/payments/dialogs/FindPatientDialog.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -40,7 +41,7 @@ public class FindPatientDialog extends BaseDialogFragment {
     private OnItemClickedListener listener;
     private String titleLabel;
     private String query;
-
+    private FrameLayout loader_container;
     /**
      * Constructor
      *
@@ -83,7 +84,7 @@ public class FindPatientDialog extends BaseDialogFragment {
         CarePayEditText findPatientEditBox = (CarePayEditText) findViewById(R.id.find_patient_edit_box);
         findPatientEditBox.addTextChangedListener(getTextChangedListener());
         findPatientEditBox.setOnKeyListener(getKeyListener());
-
+        loader_container = (FrameLayout) findViewById(R.id.loader_container);
         findViewById(R.id.find_patient_close_button).setOnClickListener(view -> {
             ((ISession) getContext()).getWorkflowServiceHelper().interrupt();
             dismiss();
@@ -136,7 +137,7 @@ public class FindPatientDialog extends BaseDialogFragment {
             public void afterTextChanged(Editable charSequence) {
                 if (charSequence.length() == 0) {
                     findViewById(R.id.patient_searched_list).setVisibility(View.GONE);
-                } else if (charSequence.length() > 3) {
+                } else if (charSequence.length() > 0) {
                     ((ISession) getContext()).getWorkflowServiceHelper().interrupt();
 
                     query = charSequence.toString().toUpperCase();
@@ -154,10 +155,14 @@ public class FindPatientDialog extends BaseDialogFragment {
 
         @Override
         public void onPreExecute() {
+         //   showProgressDialog();
+            loader_container.setVisibility(View.VISIBLE);
         }
 
         @Override
         public void onPostExecute(WorkflowDTO workflowDTO) {
+           // hideProgressDialog();
+            loader_container.setVisibility(View.INVISIBLE);
             PaymentsModel searchResult = DtoHelper.getConvertedDTO(PaymentsModel.class, workflowDTO.toString());
             if (searchResult != null) {
                 findViewById(R.id.patient_searched_list).setVisibility(View.VISIBLE);
@@ -173,6 +178,8 @@ public class FindPatientDialog extends BaseDialogFragment {
 
         @Override
         public void onFailure(String exceptionMessage) {
+           // hideProgressDialog();
+            loader_container.setVisibility(View.INVISIBLE);
             if (isAdded()) {
                 findViewById(R.id.patient_searched_list).setVisibility(View.GONE);
                 findViewById(R.id.patient_not_found_text).setVisibility(View.VISIBLE);
